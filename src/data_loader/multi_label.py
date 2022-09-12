5fde4aab8e84d8b3a05561589046bbea96d80acd
tobeor3009 <tobeor3009@gmail.com>
2022-09-12 20:26:36 +0900

    daily commit

---
+++ src/data_loader/multi_label.py
@@ -0,0 +1,95 @@
+# base module
+from tqdm import tqdm
+import os
+# external module
+import torch
+import numpy as np
+
+# this library module
+from .utils import imread, get_parent_dir_name
+from .data_utils import get_resized_array, get_seg_augumented_array, get_preprocessed_array, base_augmentation_policy_dict
+from .base_loader import BaseDataset
+
+
+class MultiDataset(BaseDataset):
+    def __init__(self,
+                 image_path_list=None,
+                 mask_path_list=None,
+                 label_policy_dict=None,
+                 label_level=1,
+                 on_memory=False,
+                 augmentation_proba=False,
+                 augmentation_policy_dict=base_augmentation_policy_dict,
+                 image_channel_dict={"image": "rgb", "mask": "grayscale"},
+                 preprocess_dict={"image": "-1~1", "mask": "0~1"},
+                 target_size=None,
+                 interpolation="bilinear",
+                 dtype=torch.float32):
+        super().__init__()
+
+        self.image_path_list = [image_path for image_path in image_path_list]
+        self.mask_path_list = [mask_path for mask_path in mask_path_list]
+
+        self.label_policy_dict = label_policy_dict
+        self.label_level = label_level
+        self.on_memory = on_memory
+        self.is_data_ready = False if on_memory else True
+        self.augmentation_proba = augmentation_proba
+        self.augmentation_policy_dict = augmentation_policy_dict
+        self.image_channel = image_channel_dict["image"]
+        self.mask_channel = image_channel_dict["mask"]
+        self.image_preprocess = preprocess_dict["image"]
+        self.mask_preprocess = preprocess_dict["mask"]
+        self.target_size = target_size
+        self.interpolation = interpolation
+        self.dtype = dtype
+
+        if self.on_memory is True:
+            self.data_on_memory_list = [None for _ in range(len(self))]
+            self.get_data_on_ram()
+
+        self.print_data_info()
+
+    def __getitem__(self, i):
+
+        if i >= len(self):
+            raise IndexError
+
+        current_index = i
+
+        if self.on_memory and self.is_data_ready:
+            image_array, mask_array = \
+                self.data_on_memory_list[current_index]
+        else:
+            image_path = self.image_path_list[current_index]
+            mask_path = self.mask_path_list[current_index]
+
+            label_str = get_parent_dir_name(image_path, level=self.label_level)
+            label_array = self.label_policy_dict[label_str]
+
+            image_array = imread(image_path, channel=self.image_channel)
+            mask_array = imread(mask_path, channel=self.mask_channel)
+
+            image_array = get_resized_array(image_array,
+                                            self.target_size,
+                                            self.interpolation)
+            mask_array = get_resized_array(mask_array,
+                                           self.target_size,
+                                           self.interpolation)
+        if (not self.on_memory) or (self.on_memory and self.is_data_ready):
+            image_array, mask_array = get_seg_augumented_array(image_array, mask_array,
+                                                               self.augmentation_proba,
+                                                               self.augmentation_policy_dict)
+            image_array = get_preprocessed_array(image_array,
+                                                 self.image_preprocess)
+            mask_array = get_preprocessed_array(mask_array,
+                                                self.mask_preprocess)
+
+            image_array = torch.as_tensor(image_array, dtype=self.dtype)
+            label_array = torch.as_tensor(label_array, dtype=self.dtype)
+            mask_array = torch.as_tensor(mask_array, dtype=self.dtype)
+        return image_array, label_array, mask_array
+
+    def print_data_info(self):
+        data_num = len(self)
+        print(f"Total data num {data_num}")
