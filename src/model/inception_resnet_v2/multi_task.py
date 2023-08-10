5e0ba44cf8c72663713433e449da17d2b9c35c96
tobeor3009 <tobeor3009@gmail.com>
2023-08-10 15:12:29 +0900

    daily commit

--- src/model/inception_resnet_v2/multi_task.py
+++ src/model/inception_resnet_v2/multi_task.py
@@ -12,9 +12,14 @@ from torch.nn import TransformerEncoder, TransformerEncoderLayer
 class InceptionResNetV2MultiTask2D(nn.Module):
     def __init__(self, input_shape, class_channel, seg_channels, block_size=16,
                  include_cbam=False, include_context=False, decode_init_channel=768,
-                 skip_connect=True, dropout_proba=0.05, class_act="softmax", seg_act="sigmoid"
+                 skip_connect=True, dropout_proba=0.05, class_act="softmax", seg_act="sigmoid",
+                 get_seg=True, get_class=True
                  ):
         super().__init__()
+
+        self.get_seg = get_seg
+        self.get_class = get_class
+
         input_shape = np.array(input_shape)
         n_input_channels = input_shape[0]
         feature_channel_num = block_size * 96
@@ -22,48 +27,58 @@ class InceptionResNetV2MultiTask2D(nn.Module):
                                        input_shape[1] // 32,
                                        input_shape[2] // 32])
         self.skip_connect = skip_connect
-        self.classfication_head = ClassificationHead(feature_channel_num,
-                                                     class_channel,
-                                                     dropout_proba, class_act)
+
         skip_connect_channel_list = get_skip_connect_channel_list(block_size)
 
         self.base_model = InceptionResNetV2_2D(n_input_channels=n_input_channels, block_size=block_size,
                                                padding="same", include_cbam=include_cbam, include_context=include_context,
                                                include_skip_connection_tensor=skip_connect)
-        for decode_i in range(0, 5):
-            decode_in_channels = decode_init_channel // (
-                2 ** (decode_i - 1)) if decode_i > 0 else feature_channel_num
-            if skip_connect:
-                decode_in_channels += skip_connect_channel_list[4 - decode_i]
-            decode_out_channels = decode_init_channel // (2 ** decode_i)
-            decode_conv = ConvBlock2D(in_channels=decode_in_channels,
-                                      out_channels=decode_out_channels, kernel_size=3)
-            decode_up = Decoder2D(in_channels=decode_out_channels,
-                                  out_channels=decode_out_channels, kernel_size=2)
-            setattr(self, f"decode_conv_{decode_i}", decode_conv)
-            setattr(self, f"decode_up_{decode_i}", decode_up)
+        if self.get_seg:
+            for decode_i in range(0, 5):
+                decode_in_channels = decode_init_channel // (
+                    2 ** (decode_i - 1)) if decode_i > 0 else feature_channel_num
+                if skip_connect:
+                    decode_in_channels += skip_connect_channel_list[4 - decode_i]
+                decode_out_channels = decode_init_channel // (2 ** decode_i)
+                decode_conv = ConvBlock2D(in_channels=decode_in_channels,
+                                          out_channels=decode_out_channels, kernel_size=3)
+                decode_up = Decoder2D(in_channels=decode_out_channels,
+                                      out_channels=decode_out_channels, kernel_size=2)
+                setattr(self, f"decode_conv_{decode_i}", decode_conv)
+                setattr(self, f"decode_up_{decode_i}", decode_up)
 
-        self.output_conv = HighwayOutput2D(in_channels=decode_out_channels,
-                                           out_channels=seg_channels,
-                                           activation=seg_act)
+            self.seg_output_conv = HighwayOutput2D(in_channels=decode_out_channels,
+                                                   out_channels=seg_channels,
+                                                   activation=seg_act)
+        if self.get_class:
+            self.classfication_head = ClassificationHead(feature_channel_num,
+                                                         class_channel,
+                                                         dropout_proba, class_act)
 
     def forward(self, input_tensor):
+        output = []
         decoded = self.base_model(input_tensor)
-        class_output = self.classfication_head(decoded)
-        for decode_i in range(0, 5):
+        if self.get_seg:
+            for decode_i in range(0, 5):
 
-            if self.skip_connect:
-                skip_connect_tensor = getattr(
-                    self.base_model, f"skip_connect_tensor_{4 - decode_i}")
-                decoded = torch.cat([decoded, skip_connect_tensor], axis=1)
+                if self.skip_connect:
+                    skip_connect_tensor = getattr(
+                        self.base_model, f"skip_connect_tensor_{4 - decode_i}")
+                    decoded = torch.cat([decoded, skip_connect_tensor], axis=1)
 
-            decode_conv = getattr(self, f"decode_conv_{decode_i}")
-            decode_up = getattr(self, f"decode_up_{decode_i}")
-            decoded = decode_conv(decoded)
-            decoded = decode_up(decoded)
+                decode_conv = getattr(self, f"decode_conv_{decode_i}")
+                decode_up = getattr(self, f"decode_up_{decode_i}")
+                decoded = decode_conv(decoded)
+                decoded = decode_up(decoded)
+            seg_output = self.seg_output_conv(decoded)
+            output.append(seg_output)
 
-        seg_output = self.output_conv(decoded)
-        return seg_output, class_output
+        if self.get_class:
+            class_output = self.classfication_head(decoded)
+            output.append(class_output)
+        if len(output) == 1:
+            output = output[0]
+        return output
 
 
 class PositionalEncoding(nn.Module):
