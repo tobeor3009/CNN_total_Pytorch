fdb594a09f8bb8fde0b0866d15272636d888dfb8
tobeor3009 <tobeor3009@gmail.com>
2023-09-14 23:24:24 +0900

    daily commit

--- src/model/inception_resnet_v2/layers.py
+++ src/model/inception_resnet_v2/layers.py
@@ -2,8 +2,8 @@ import math
 from functools import partial
 import torch
 from torch import nn
+from torch.nn import functional as F
 from .cbam import CBAM
-
 INPLACE = False
 DEFAULT_ACT = "mish"
 
@@ -28,6 +28,60 @@ def get_act(activation):
     elif activation is None:
         act = nn.Identity()
     return act
+
+
+def space_to_depth(x, block_size):
+    n, c, h, w = x.size()
+    unfolded_x = F.unfold(x, block_size, stride=block_size)
+    return unfolded_x.view(n, c * block_size ** 2,
+                           h // block_size, w // block_size)
+
+
+def space_to_depth_3d(input_tensor, block_size):
+    B, C, D, H, W = input_tensor.shape
+
+    # Reshape tensor
+    output_tensor = input_tensor.view(B, C,
+                                      D // block_size, block_size,
+                                      H // block_size, block_size,
+                                      W // block_size, block_size)
+
+    # Permute tensor dimensions
+    output_tensor = output_tensor.permute(0, 1, 3, 5, 7, 2, 4, 6).contiguous()
+
+    # Merge block_size^3 blocks into the channels
+    output_tensor = output_tensor.view(B, C * block_size * block_size * block_size,
+                                       D // block_size, H // block_size, W // block_size)
+    return output_tensor
+
+
+class PixelShuffle3d(nn.Module):
+    '''
+    Source: https://github.com/kuoweilai/pixelshuffle3d/blob/master/pixelshuffle3d.py
+    This class is a 3d version of pixelshuffle.
+    '''
+
+    def __init__(self, scale):
+        '''
+        :param scale: upsample scale
+        '''
+        super().__init__()
+        self.scale = scale
+
+    def forward(self, input):
+        batch_size, channels, in_depth, in_height, in_width = input.size()
+        nOut = channels // self.scale ** 3
+
+        out_depth = in_depth * self.scale
+        out_height = in_height * self.scale
+        out_width = in_width * self.scale
+
+        input_view = input.contiguous().view(batch_size, nOut, self.scale, self.scale,
+                                             self.scale, in_depth, in_height, in_width)
+
+        output = input_view.permute(0, 1, 5, 2, 6, 3, 7, 4).contiguous()
+
+        return output.view(batch_size, nOut, out_depth, out_height, out_width)
 
 
 class ConvBlock2D(nn.Module):
--- src/model/inception_resnet_v2/multi_task_3d.py
+++ src/model/inception_resnet_v2/multi_task_3d.py
@@ -3,15 +3,15 @@ import math
 from torch import nn
 import numpy as np
 from .layers import get_act
-from .base_model import InceptionResNetV2_2D, get_skip_connect_channel_list
+from .base_model import InceptionResNetV2_3D, get_skip_connect_channel_list
 from .transformer_layers import PositionalEncoding
-from .layers import space_to_depth
-from .layers import ConvBlock2D, Decoder2D, HighwayOutput2D
+from .layers import space_to_depth_3d
+from .layers import ConvBlock3D, Decoder3D, HighwayOutput3D
 from torch.nn import TransformerEncoder, TransformerEncoderLayer
 USE_INPLACE = True
 
 
-class InceptionResNetV2MultiTask2D(nn.Module):
+class InceptionResNetV2MultiTask3D(nn.Module):
     def __init__(self, input_shape, class_channel, seg_channels, block_size=16,
                  include_cbam=False, include_context=False, decode_init_channel=768,
                  skip_connect=True, dropout_proba=0.05, class_act="softmax", seg_act="sigmoid",
@@ -27,12 +27,13 @@ class InceptionResNetV2MultiTask2D(nn.Module):
         feature_channel_num = block_size * 96
         self.feature_shape = np.array([feature_channel_num,
                                        input_shape[1] // 32,
-                                       input_shape[2] // 32])
+                                       input_shape[2] // 32,
+                                       input_shape[3] // 32])
         self.skip_connect = skip_connect
 
         skip_connect_channel_list = get_skip_connect_channel_list(block_size)
 
-        self.base_model = InceptionResNetV2_2D(n_input_channels=n_input_channels, block_size=block_size,
+        self.base_model = InceptionResNetV2_3D(n_input_channels=n_input_channels, block_size=block_size,
                                                padding="same", include_cbam=include_cbam, include_context=include_context,
                                                include_skip_connection_tensor=skip_connect)
         if self.get_seg:
@@ -42,14 +43,14 @@ class InceptionResNetV2MultiTask2D(nn.Module):
                 if skip_connect:
                     decode_in_channels += skip_connect_channel_list[4 - decode_i]
                 decode_out_channels = decode_init_channel // (2 ** decode_i)
-                decode_conv = ConvBlock2D(in_channels=decode_in_channels,
+                decode_conv = ConvBlock3D(in_channels=decode_in_channels,
                                           out_channels=decode_out_channels, kernel_size=3)
-                decode_up = Decoder2D(in_channels=decode_out_channels,
+                decode_up = Decoder3D(in_channels=decode_out_channels,
                                       out_channels=decode_out_channels, kernel_size=2)
                 setattr(self, f"decode_conv_{decode_i}", decode_conv)
                 setattr(self, f"decode_up_{decode_i}", decode_up)
 
-            self.seg_output_conv = HighwayOutput2D(in_channels=decode_out_channels,
+            self.seg_output_conv = HighwayOutput3D(in_channels=decode_out_channels,
                                                    out_channels=seg_channels,
                                                    activation=seg_act)
         if self.get_class:
@@ -110,8 +111,8 @@ class PositionalEncoding(nn.Module):
 class ClassificationHeadSimple(nn.Module):
     def __init__(self, in_channels, num_classes, dropout_proba, activation):
         super(ClassificationHeadSimple, self).__init__()
-        self.gap_layer = nn.AdaptiveAvgPool2d((2, 2))
-        self.fc_1 = nn.Linear(in_channels * 4, in_channels)
+        self.gap_layer = nn.AdaptiveAvgPool3d((2, 2, 2))
+        self.fc_1 = nn.Linear(in_channels * 8, in_channels)
         self.dropout_layer = nn.Dropout(p=dropout_proba, inplace=USE_INPLACE)
         self.relu_layer = nn.ReLU6(inplace=USE_INPLACE)
         self.fc_2 = nn.Linear(in_channels, num_classes)
@@ -132,9 +133,9 @@ class ClassificationHead(nn.Module):
     def __init__(self, in_channels, num_classes, dropout_proba, activation):
         super(ClassificationHead, self).__init__()
         self.in_channels = in_channels
-        self.pos_encoder = PositionalEncoding(in_channels // 4)
+        self.pos_encoder = PositionalEncoding(in_channels // 8)
         self.pixel_shffle = torch.nn.PixelShuffle(2)
-        encoder_layers = TransformerEncoderLayer(d_model=in_channels // 4, nhead=8,
+        encoder_layers = TransformerEncoderLayer(d_model=in_channels // 8, nhead=8,
                                                  dropout=dropout_proba)
         self.transformer_encoder = TransformerEncoder(encoder_layers,
                                                       num_layers=6)
@@ -143,25 +144,26 @@ class ClassificationHead(nn.Module):
         self.act = get_act(activation)
 
     def forward(self, x):
-        batch_size, in_channel, h, w = x.shape
+        batch_size, in_channel, z, h, w = x.shape
         # Assumes x has shape [N, C, H, W]
         x = self.pixel_shffle(x)
-        # shape: [N, C // 4, H * 2,  W * 2]
-        x = x.view(batch_size, self.in_channels // 4, -1)
-        # shape: [N, C // 4, H * W * 4]
+        # shape: [N, C // 8, Z * 2, H * 2,  W * 2]
+        x = x.view(batch_size, self.in_channels // 8, -1)
+        # shape: [N, C // 8, Z * H * W * 8]
         x = x.permute(0, 2, 1)
-        # shape: [N, H * W * 4, C // 4]
+        # shape: [N, Z * H * W * 8, C // 8]
         x = self.pos_encoder(x)
         x = self.transformer_encoder(x)
         x = self.dropout(x)
-        # shape: [N, H * W * 4, C // 4]
-        x = x.view(batch_size, h * 2, w * 2, in_channel // 4)
-        # shape: [N, H * 2, W * 2, C // 4]
-        x = x.permute(0, 3, 1, 2)
-        # shape: [N, C // 4, H * 2, W * 2]
-        x = space_to_depth(x, 2)
-        # shape: [N, C, H, W]
-        x = x.mean([2, 3])
+        # shape: [N, Z * H * W * 8, C // 8]
+        x = x.view(batch_size, z * 2, h * 2, w * 2,
+                   in_channel // 8)
+        # shape: [N, Z * 2, H * 2, W * 2, C // 8]
+        x = x.permute(0, 4, 1, 2, 3)
+        # shape: [N, C // 8, Z * 2, H * 2, W * 2]
+        x = space_to_depth_3d(x, 2)
+        # shape: [N, C, Z, H, W]
+        x = x.mean([2, 3, 4])
         # shape: [N, C]
         x = self.fc(x)
         x = self.act(x)
