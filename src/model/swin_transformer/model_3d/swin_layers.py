f482dd6ed89f1c759b0fa968440bffff227da8ee
tobeor3009 <tobeor3009@gmail.com>
2023-11-06 16:50:23 +0900

    fix swin 3d's mlp

--- src/model/swin_transformer/model_3d/swin_layers.py
+++ src/model/swin_transformer/model_3d/swin_layers.py
@@ -16,7 +16,7 @@ class Mlp(nn.Module):
         out_features = out_features or in_features
         hidden_features = hidden_features or in_features
         self.fc1 = nn.Linear(in_features, hidden_features)
-        self.act = act_layer()
+        self.act = act_layer
         self.fc2 = nn.Linear(hidden_features, out_features)
         self.drop = nn.Dropout(drop, inplace=DROPOUT_INPLACE)
 
