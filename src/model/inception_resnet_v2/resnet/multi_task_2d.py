d543a4f0a85d0a8e46031bfe5a6be48997ce80e2
tobeor3009 <tobeor3009@gmail.com>
2023-11-12 16:37:11 +0900

    add 2d resnet model

---
+++ src/model/inception_resnet_v2/resnet/multi_task_2d.py
@@ -0,0 +1,228 @@
+import torch
+import math
+from torch import nn
+import numpy as np
+from ..layers import get_act, get_norm
+from timm.models.layers import trunc_normal_
+from .resnet_2d import resnet
+from ..transformer_layers import PositionalEncoding
+from ..layers import ConvBlock2D, AttentionPool, Output2D
+from ..layers_highway import MultiDecoder2D, HighwayOutput2D
+USE_INPLACE = True
+
+
+class ResNetMultiTask2D(nn.Module):
+    def __init__(self, input_shape, class_channel=None, seg_channels=None, validity_shape=(1, 8, 8),
+                 block_size=64, block_depth_list=[3, 4, 6, 3],
+                 inject_class_channel=None, decode_init_channel=None, dropout_proba=0.05,
+                 class_act="softmax", seg_act="sigmoid", validity_act="sigmoid",
+                 get_seg=True, get_class=True, get_validity=False,
+                 use_class_head_simple=True,
+                 use_seg_pixelshuffle_only=True, use_seg_simpleoutput=False
+                 ):
+        super().__init__()
+
+        self.get_seg = get_seg
+        self.get_class = get_class
+        self.get_validity = get_validity
+        self.inject_class_channel = inject_class_channel
+        if decode_init_channel is None:
+            decode_init_channel = block_size * 16
+        skip_connect_channel_list = [block_size,
+                                     block_size * 4,
+                                     block_size * 8,
+                                     block_size * 16]
+        input_shape = np.array(input_shape)
+        n_input_channels, init_h, init_w = input_shape
+        feature_h, feature_w = (init_h // (2 ** 5),
+                                init_w // (2 ** 5),)
+        feature_channel_num = block_size * 32
+
+        self.feature_shape = np.array([feature_channel_num,
+                                       input_shape[1] // 32,
+                                       input_shape[2] // 32])
+
+        self.base_model = resnet(in_channel=n_input_channels,
+                                 block_size=block_size,
+                                 block_depth_list=block_depth_list)
+        if self.get_class:
+            if use_class_head_simple:
+                self.classfication_head = ClassificationHeadSimple(feature_channel_num,
+                                                                   class_channel,
+                                                                   dropout_proba, class_act)
+            else:
+                self.classfication_head = ClassificationHead((feature_h, feature_w),
+                                                             feature_channel_num,
+                                                             class_channel,
+                                                             dropout_proba, class_act)
+        if self.get_seg:
+            self.decode_init_conv = ConvBlock2D(in_channels=feature_channel_num,
+                                                out_channels=decode_init_channel, kernel_size=1)
+            for decode_i in range(0, 5):
+                h, w = (init_h // (2 ** (5 - decode_i)),
+                        init_w // (2 ** (5 - decode_i)))
+                decode_in_channels = int(decode_init_channel //
+                                         (2 ** decode_i))
+                if decode_i > 0:
+                    skip_conv = nn.Conv2d(in_channels=(decode_in_channels +
+                                                       skip_connect_channel_list[-decode_i]),
+                                          out_channels=decode_in_channels, kernel_size=1)
+                    setattr(self, f"decode_skip_conv_{decode_i}", skip_conv)
+
+                decode_out_channels = decode_in_channels // 2
+                decode_conv = ConvBlock2D(in_channels=decode_in_channels,
+                                          out_channels=decode_out_channels,
+                                          kernel_size=3,
+                                          groups=4 if decode_i == 0 else 1)
+                decode_kernel_size = (2, 2, 2)
+                if decode_i == 0:
+                    use_pixelshuffle_only = True
+                else:
+                    use_pixelshuffle_only = use_seg_pixelshuffle_only
+                decode_up = MultiDecoder2D(input_zhw=(h, w),
+                                           in_channels=decode_out_channels,
+                                           out_channels=decode_out_channels,
+                                           kernel_size=decode_kernel_size,
+                                           use_highway=False,
+                                           use_pixelshuffle_only=use_pixelshuffle_only)
+                setattr(self, f"decode_conv_{decode_i}", decode_conv)
+                setattr(self, f"decode_up_{decode_i}", decode_up)
+            if use_seg_simpleoutput:
+                self.seg_output_conv = Output2D(in_channels=decode_out_channels,
+                                                out_channels=seg_channels,
+                                                act=seg_act)
+            else:
+                self.seg_output_conv = HighwayOutput2D(in_channels=decode_out_channels,
+                                                       out_channels=seg_channels,
+                                                       act=seg_act, use_highway=False)
+        if get_validity:
+            self.validity_conv_1 = ConvBlock2D(feature_channel_num, block_size * 2,
+                                               kernel_size=3, act="gelu", norm=None)
+            self.validity_avg_pool = nn.AdaptiveAvgPool2d(validity_shape[1:])
+            self.validity_out_conv = ConvBlock2D(block_size * 2, validity_shape[0],
+                                                 kernel_size=1, act=validity_act, norm=None)
+        if inject_class_channel is not None and get_seg:
+            self.inject_linear = nn.Linear(inject_class_channel,
+                                           decode_init_channel, bias=False)
+            self.inject_norm = get_norm("layer", decode_init_channel, "2d")
+            inject_pos_embed_shape = torch.zeros(1, 1,
+                                                 *self.feature_shape[1:],
+                                                 )
+            self.inject_absolute_pos_embed = nn.Parameter(
+                inject_pos_embed_shape)
+            trunc_normal_(self.inject_absolute_pos_embed, std=.02)
+            self.inject_cat_conv = nn.Conv2d(decode_init_channel * 2,
+                                             decode_init_channel, kernel_size=1, padding=0, bias=False)
+
+    def validity_forward(self, x):
+        x = self.validity_conv_1(x)
+        x = self.validity_avg_pool(x)
+        x = self.validity_out_conv(x)
+        return x
+
+    def forward(self, input_tensor, inject_class=None):
+        output = []
+        encode_feature, skip_connect_list = self.base_model(input_tensor)
+        if self.get_seg:
+            decoded = encode_feature
+            decoded = self.decode_init_conv(decoded)
+            if self.inject_class_channel is not None:
+                inject_class = self.inject_linear(inject_class)
+                inject_class = self.inject_norm(inject_class)
+                inject_class = inject_class[:, :, None, None, None]
+                inject_class = inject_class.repeat(1, 1,
+                                                   decoded.shape[2],
+                                                   decoded.shape[3])
+                inject_class = inject_class + self.inject_absolute_pos_embed
+                decoded = torch.cat([decoded, inject_class], dim=1)
+                decoded = self.inject_cat_conv(decoded)
+
+            for decode_i in range(0, 5):
+                if decode_i > 0:
+                    skip_connect_tensor = skip_connect_list[-decode_i]
+                    decoded = torch.cat([decoded,
+                                        skip_connect_tensor], dim=1)
+                    decoded_skip_conv = getattr(self,
+                                                f"decode_skip_conv_{decode_i}")
+                    decoded = decoded_skip_conv(decoded)
+                decode_conv = getattr(self, f"decode_conv_{decode_i}")
+                decode_up = getattr(self, f"decode_up_{decode_i}")
+                decoded = decode_conv(decoded)
+                decoded = decode_up(decoded)
+            seg_output = self.seg_output_conv(decoded)
+            output.append(seg_output)
+
+        if self.get_class:
+            class_output = self.classfication_head(encode_feature)
+            output.append(class_output)
+
+        if self.get_validity:
+            validity_output = self.validity_forward(encode_feature)
+            output.append(validity_output)
+        if len(output) == 1:
+            output = output[0]
+        return output
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout, inplace=USE_INPLACE)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(
+            0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + self.pe[:x.size(0), :]
+        return self.dropout(x)
+
+
+class ClassificationHeadSimple(nn.Module):
+    def __init__(self, in_channels, num_classes, dropout_proba, act):
+        super(ClassificationHeadSimple, self).__init__()
+        self.gap_layer = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc_1 = nn.Linear(in_channels, in_channels // 2)
+        self.dropout_layer = nn.Dropout(p=dropout_proba, inplace=USE_INPLACE)
+        self.relu_layer = nn.ReLU6(inplace=USE_INPLACE)
+        self.fc_2 = nn.Linear(in_channels // 2, num_classes)
+        self.act = get_act(act)
+
+    def forward(self, x):
+        # [B C H W]
+        x = self.gap_layer(x)
+        # [B C 1 1]
+        x = x.flatten(start_dim=1, end_dim=-1)
+        # [B C]
+        x = self.fc_1(x)
+        # [B C // 2]
+        x = self.dropout_layer(x)
+        # [B C // 2]
+        x = self.relu_layer(x)
+        # [B C // 2]
+        x = self.fc_2(x)
+        # [B num_class]
+        x = self.act(x)
+        return x
+
+
+class ClassificationHead(nn.Module):
+    def __init__(self, feature_zhw, in_channels, num_classes, dropout_proba, act):
+        super(ClassificationHead, self).__init__()
+        self.attn_pool = AttentionPool(feature_num=np.prod(feature_zhw), embed_dim=in_channels,
+                                       num_heads=4, output_dim=in_channels * 2)
+        self.dropout = nn.Dropout(p=dropout_proba, inplace=USE_INPLACE)
+        self.fc = nn.Linear(in_channels * 2, num_classes)
+        self.act = get_act(act)
+
+    def forward(self, x):
+        x = self.attn_pool(x)
+        x = self.dropout(x)
+        x = self.fc(x)
+        x = self.act(x)
+        return x
---
+++ src/model/inception_resnet_v2/resnet/resnet_2d.py
@@ -0,0 +1,333 @@
+from types import FunctionType
+from typing import Any, Callable, List, Optional, Type, Union
+import torch
+from torch import Tensor
+from torch import nn
+from ._api import WeightsEnum
+from ._utils import _ovewrite_named_param
+
+
+def _log_api_usage_once(obj: Any) -> None:
+    """
+    Logs API usage(module and name) within an organization.
+    In a large ecosystem, it's often useful to track the PyTorch and
+    TorchVision APIs usage. This API provides the similar functionality to the
+    logging module in the Python stdlib. It can be used for debugging purpose
+    to log which methods are used and by default it is inactive, unless the user
+    manually subscribes a logger via the `SetAPIUsageLogger method <https://github.com/pytorch/pytorch/blob/eb3b9fe719b21fae13c7a7cf3253f970290a573e/c10/util/Logging.cpp#L114>`_.
+    Please note it is triggered only once for the same API call within a process.
+    It does not collect any data from open-source users since it is no-op by default.
+    For more information, please refer to
+    * PyTorch note: https://pytorch.org/docs/stable/notes/large_scale_deployments.html#api-usage-logging;
+    * Logging policy: https://github.com/pytorch/vision/issues/5052;
+
+    Args:
+        obj (class instance or method): an object to extract info from.
+    """
+    module = obj.__module__
+    if not module.startswith("torchvision"):
+        module = f"torchvision.internal.{module}"
+    name = obj.__class__.__name__
+    if isinstance(obj, FunctionType):
+        name = obj.__name__
+    torch._C._log_api_usage_once(f"{module}.{name}")
+
+
+def conv3x3(in_planes: int, out_planes: int, stride: int = 1,
+            groups: int = 1, dilation: int = 1) -> nn.Conv2d:
+    """3x3 convolution with padding"""
+    return nn.Conv2d(
+        in_planes,
+        out_planes,
+        kernel_size=3,
+        stride=stride,
+        padding=dilation,
+        groups=groups,
+        bias=False,
+        dilation=dilation,
+    )
+
+
+def conv1x1(in_planes: int, out_planes: int, stride: int = 1) -> nn.Conv2d:
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes,
+                     kernel_size=1, stride=stride, bias=False)
+
+
+class BasicBlock(nn.Module):
+    expansion: int = 1
+
+    def __init__(
+        self,
+        inplanes: int,
+        planes: int,
+        stride: int = 1,
+        downsample: Optional[nn.Module] = None,
+        groups: int = 1,
+        base_width: int = 64,
+        dilation: int = 1,
+        norm_layer: Optional[Callable[..., nn.Module]] = None,
+    ) -> None:
+        super().__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        if groups != 1 or base_width != 64:
+            raise ValueError(
+                "BasicBlock only supports groups=1 and base_width=64")
+        if dilation > 1:
+            raise NotImplementedError(
+                "Dilation > 1 not supported in BasicBlock")
+        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = norm_layer(planes)
+        self.relu = nn.ReLU6(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = norm_layer(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x: Tensor) -> Tensor:
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class Bottleneck(nn.Module):
+    # Bottleneck in torchvision places the stride for downsampling at 3x3 convolution(self.conv2)
+    # while original implementation places the stride at the first 1x1 convolution(self.conv1)
+    # according to "Deep residual learning for image recognition" https://arxiv.org/abs/1512.03385.
+    # This variant is also known as ResNet V1.5 and improves accuracy according to
+    # https://ngc.nvidia.com/catalog/model-scripts/nvidia:resnet_50_v1_5_for_pytorch.
+
+    expansion: int = 4
+
+    def __init__(
+        self,
+        inplanes: int,
+        planes: int,
+        stride: int = 1,
+        downsample: Optional[nn.Module] = None,
+        groups: int = 1,
+        base_width: int = 64,
+        dilation: int = 1,
+        norm_layer: Optional[Callable[..., nn.Module]] = None,
+    ) -> None:
+        super().__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        width = int(planes * (base_width / 64.0)) * groups
+        # Both self.conv2 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv1x1(inplanes, width)
+        self.bn1 = norm_layer(width)
+        self.conv2 = conv3x3(width, width, stride, groups, dilation)
+        self.bn2 = norm_layer(width)
+        self.conv3 = conv1x1(width, planes * self.expansion)
+        self.bn3 = norm_layer(planes * self.expansion)
+        self.relu = nn.ReLU6(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x: Tensor) -> Tensor:
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class ResNet2d(nn.Module):
+    def __init__(
+        self,
+        block: Type[Union[BasicBlock, Bottleneck]],
+        block_size: int,
+        layers: List[int],
+        in_channel: int = 3,
+        zero_init_residual: bool = False,
+        groups: int = 1,
+        width_per_group: int = 64,
+        replace_stride_with_dilation: Optional[List[bool]] = None,
+        norm_layer: Optional[Callable[..., nn.Module]] = None,
+    ) -> None:
+        super().__init__()
+        _log_api_usage_once(self)
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        self._norm_layer = norm_layer
+
+        self.inplanes = block_size
+        self.dilation = 1
+        if replace_stride_with_dilation is None:
+            # each element in the tuple indicates if we should replace
+            # the 2x2 stride with a dilated convolution instead
+            replace_stride_with_dilation = [False, False, False]
+        if len(replace_stride_with_dilation) != 3:
+            raise ValueError(
+                "replace_stride_with_dilation should be None "
+                f"or a 3-element tuple, got {replace_stride_with_dilation}"
+            )
+        self.groups = groups
+        self.base_width = width_per_group
+        self.conv1 = nn.Conv2d(in_channel, self.inplanes,
+                               kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = norm_layer(self.inplanes)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, block_size, layers[0])
+        self.layer2 = self._make_layer(block, block_size * 2, layers[1],
+                                       stride=2, dilate=replace_stride_with_dilation[0])
+        self.layer3 = self._make_layer(block, block_size * 4, layers[2],
+                                       stride=2, dilate=replace_stride_with_dilation[1])
+        self.layer4 = self._make_layer(block, block_size * 8, layers[3],
+                                       stride=2, dilate=replace_stride_with_dilation[2])
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(
+                    m.weight, mode="fan_out", nonlinearity="relu")
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, Bottleneck) and m.bn3.weight is not None:
+                    # type: ignore[arg-type]
+                    nn.init.constant_(m.bn3.weight, 0)
+                elif isinstance(m, BasicBlock) and m.bn2.weight is not None:
+                    # type: ignore[arg-type]
+                    nn.init.constant_(m.bn2.weight, 0)
+
+    def _make_layer(
+        self,
+        block: Type[Union[BasicBlock, Bottleneck]],
+        planes: int,
+        blocks: int,
+        stride: int = 1,
+        dilate: bool = False,
+    ) -> nn.Sequential:
+        norm_layer = self._norm_layer
+        downsample = None
+        previous_dilation = self.dilation
+        if dilate:
+            self.dilation *= stride
+            stride = 1
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                norm_layer(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(
+            block(
+                self.inplanes, planes, stride, downsample, self.groups, self.base_width, previous_dilation, norm_layer
+            )
+        )
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(
+                block(
+                    self.inplanes,
+                    planes,
+                    groups=self.groups,
+                    base_width=self.base_width,
+                    dilation=self.dilation,
+                    norm_layer=norm_layer,
+                )
+            )
+
+        return nn.Sequential(*layers)
+
+    def _forward_impl(self, x: Tensor) -> Tensor:
+        skip_connect_list = []
+        # See note [TorchScript super()]
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        skip_connect_list.append(x)
+        x = self.maxpool(x)
+        x = self.layer1(x)
+        skip_connect_list.append(x)
+        x = self.layer2(x)
+        skip_connect_list.append(x)
+        x = self.layer3(x)
+        skip_connect_list.append(x)
+        x = self.layer4(x)
+        return x, skip_connect_list
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self._forward_impl(x)
+
+
+def _resnet(
+    block: Type[Union[BasicBlock, Bottleneck]],
+    block_size: int,
+    layers: List[int],
+    in_channel: int,
+    weights: Optional[WeightsEnum],
+    progress: bool,
+    **kwargs: Any,
+) -> ResNet2d:
+    if weights is not None:
+        _ovewrite_named_param(kwargs, "num_classes",
+                              len(weights.meta["categories"]))
+
+    model = ResNet2d(block, block_size, layers, in_channel, **kwargs)
+
+    if weights is not None:
+        model.load_state_dict(weights.get_state_dict(
+            progress=progress, check_hash=True))
+
+    return model
+
+
+def resnet(in_channel=3, block_size=64, block_depth_list=[1, 2, 3, 1],
+           progress: bool = True, **kwargs: Any) -> ResNet2d:
+    weights = None
+    return _resnet(Bottleneck, block_size, block_depth_list, in_channel, weights, progress, **kwargs)
+
+
+def resnet_tiny(in_channel=3, progress: bool = True, **kwargs: Any) -> ResNet2d:
+    weights = None
+    return _resnet(Bottleneck, 64, [1, 2, 3, 1], in_channel, weights, progress, **kwargs)
+
+
+def resnet50(in_channel=3, progress: bool = True, **kwargs: Any) -> ResNet2d:
+    weights = None
+    return _resnet(Bottleneck, 64, [3, 4, 6, 3], in_channel, weights, progress, **kwargs)
+
+
+def resnet101(in_channel=3, progress: bool = True, **kwargs: Any) -> ResNet2d:
+    weights = None
+    return _resnet(Bottleneck, 64, [3, 4, 23, 3], in_channel, weights, progress, **kwargs)
