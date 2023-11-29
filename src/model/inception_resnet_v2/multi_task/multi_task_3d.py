0a16160db5145dba2479f34d859db0b2d2408910
tobeor3009 <tobeor3009@gmail.com>
2023-11-29 17:25:31 +0900

    fix typo

--- src/model/inception_resnet_v2/multi_task/multi_task_3d.py
+++ src/model/inception_resnet_v2/multi_task/multi_task_3d.py
@@ -100,7 +100,7 @@ class InceptionResNetV2MultiTask3D(nn.Module):
                                                              dropout_proba, class_act)
         if get_validity:
             self.validity_conv_1 = ConvBlock3D(feature_channel_num, block_size * 8,
-                                               kernel_size=3, act="gelu", norm=ac)
+                                               kernel_size=3, act="gelu", norm=norm)
             self.validity_avg_pool = nn.AdaptiveAvgPool3d(validity_shape[1:])
             self.validity_out_conv = ConvBlock3D(block_size * 8, validity_shape[0],
                                                  kernel_size=3, act=validity_act, norm=norm)
