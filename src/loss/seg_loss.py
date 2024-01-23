cb0d4d908c933a6b23f2e222e881b013896c2bc4
tobeor3009 <tobeor3009@gmail.com>
2024-01-23 15:29:21 +0900

    daily commit

--- src/loss/seg_loss.py
+++ src/loss/seg_loss.py
@@ -1,14 +1,15 @@
 import torch
 import torch.optim.lr_scheduler as lr_scheduler
+from functools import partial
 AXIS = [2, 3]
 SMOOTH = 1e-7
 
 
-def get_bce_loss(y_pred, y_true, per_image=False):
-    y_true = torch.clamp(y_true, min=SMOOTH)
-    y_pred = torch.clamp(y_pred, min=SMOOTH)
-    y_true_reverse = torch.clamp(1 - y_true, min=SMOOTH)
-    y_pred_reverse = torch.clamp(1 - y_pred, min=SMOOTH)
+def get_bce_loss(y_pred, y_true, per_image=False, smooth=SMOOTH):
+    y_true = torch.clamp(y_true, min=smooth)
+    y_pred = torch.clamp(y_pred, min=smooth)
+    y_true_reverse = torch.clamp(1 - y_true, min=smooth)
+    y_pred_reverse = torch.clamp(1 - y_pred, min=smooth)
     gt_negative_term = -(y_true_reverse) * torch.log(y_pred_reverse)
     gt_positive_term = -y_true * torch.log(y_pred)
     per_image_loss = torch.mean(gt_positive_term + gt_negative_term, dim=AXIS)
@@ -18,12 +19,12 @@ def get_bce_loss(y_pred, y_true, per_image=False):
         return torch.mean(per_image_loss)
 
 
-def get_focal_loss(y_pred, y_true, per_image=False, gamma=2.0):
+def get_focal_loss(y_pred, y_true, per_image=False, gamma=2.0, smooth=SMOOTH):
     batch_size = y_pred.size(0)
-    y_true = torch.clamp(y_true, min=SMOOTH)
-    y_pred = torch.clamp(y_pred, min=SMOOTH)
-    y_true_reverse = torch.clamp(1 - y_true, min=SMOOTH)
-    y_pred_reverse = torch.clamp(1 - y_pred, min=SMOOTH)
+    y_true = torch.clamp(y_true, min=smooth)
+    y_pred = torch.clamp(y_pred, min=smooth)
+    y_true_reverse = torch.clamp(1 - y_true, min=smooth)
+    y_pred_reverse = torch.clamp(1 - y_pred, min=smooth)
     gt_negative_term = -(y_pred ** gamma) * \
         (y_true_reverse) * torch.log(y_pred_reverse)
     gt_positive_term = -(y_pred_reverse ** gamma) * y_true * torch.log(y_pred)
@@ -34,10 +35,10 @@ def get_focal_loss(y_pred, y_true, per_image=False, gamma=2.0):
         return torch.mean(per_image_loss)
 
 
-def get_dice_loss(y_pred, y_true, log=False, per_image=False, smooth=SMOOTH):
-    tp = torch.sum(y_true * y_pred, axis=AXIS)
-    fp = torch.sum(y_pred, axis=AXIS) - tp
-    fn = torch.sum(y_true, axis=AXIS) - tp
+def get_dice_loss(y_pred, y_true, axis=AXIS, log=False, per_image=False, smooth=SMOOTH):
+    tp = torch.sum(y_true * y_pred, axis=axis)
+    fp = torch.sum(y_pred, axis=axis) - tp
+    fn = torch.sum(y_true, axis=axis) - tp
     dice_score_per_image = (2 * tp + smooth) / (2 * tp + fp + fn + smooth)
     if log:
         dice_score_per_image = -1 * torch.log(dice_score_per_image)
@@ -50,11 +51,11 @@ def get_dice_loss(y_pred, y_true, log=False, per_image=False, smooth=SMOOTH):
         return torch.mean(dice_score_per_image)
 
 
-def get_tversky_loss(y_pred, y_true, beta=0.7, log=False, per_image=False, smooth=SMOOTH):
+def get_tversky_loss(y_pred, y_true, beta=0.7, axis=AXIS, log=False, per_image=False, smooth=SMOOTH):
     alpha = 1 - beta
-    tp = torch.sum(y_true * y_pred, axis=AXIS)
-    fp = torch.sum(y_pred, axis=AXIS) - tp
-    fn = torch.sum(y_true, axis=AXIS) - tp
+    tp = torch.sum(y_true * y_pred, axis=axis)
+    fp = torch.sum(y_pred, axis=axis) - tp
+    fn = torch.sum(y_true, axis=axis) - tp
     dice_score_per_image = (tp + smooth) / \
         (tp + alpha * fp + beta * fn + smooth)
     if log:
@@ -68,15 +69,15 @@ def get_tversky_loss(y_pred, y_true, beta=0.7, log=False, per_image=False, smoot
         return torch.mean(dice_score_per_image)
 
 
-def get_propotional_loss(y_pred, y_true, log=False, per_image=False, smooth=SMOOTH, beta=0.9):
+def get_propotional_loss(y_pred, y_true, axis=AXIS, log=False, per_image=False, smooth=SMOOTH, beta=0.9):
 
     alpha = 1 - beta
-    prevalence = torch.mean(y_true, dim=AXIS)
+    prevalence = torch.mean(y_true, dim=axis)
 
-    tp = torch.sum(y_true * y_pred, dim=AXIS)
-    tn = torch.sum((1 - y_true) * (1 - y_pred), dim=AXIS)
-    fp = torch.sum(y_pred, dim=AXIS) - tp
-    fn = torch.sum(y_true, dim=AXIS) - tp
+    tp = torch.sum(y_true * y_pred, dim=axis)
+    tn = torch.sum((1 - y_true) * (1 - y_pred), dim=axis)
+    fp = torch.sum(y_pred, dim=axis) - tp
+    fn = torch.sum(y_true, dim=axis) - tp
     negative_score = (tn + smooth) \
         / (tn + beta * fn + alpha * fp + smooth) * (smooth + 1 - prevalence)
     positive_score = (tp + smooth) \
@@ -94,31 +95,46 @@ def get_propotional_loss(y_pred, y_true, log=False, per_image=False, smooth=SMOO
         return torch.mean(score_per_image)
 
 
-def get_dice_bce_loss(y_pred, y_true):
-    return get_bce_loss(y_pred, y_true) + get_dice_loss(y_pred, y_true)
+def get_dice_bce_loss(y_pred, y_true, axis=AXIS):
+    return get_bce_loss(y_pred, y_true, axis=axis) + get_dice_loss(y_pred, y_true, axis=axis)
+
+def get_tversky_bce_loss(y_pred, y_true, axis=AXIS):
+    return get_bce_loss(y_pred, y_true, axis=axis) + get_tversky_loss(y_pred, y_true, axis=axis)
 
 
-def get_tversky_bce_loss(y_pred, y_true):
-    return get_bce_loss(y_pred, y_true) + get_tversky_loss(y_pred, y_true)
+def get_propotional_bce_loss(y_pred, y_true, axis=AXIS):
+    return get_bce_loss(y_pred, y_true, axis=axis) + get_propotional_loss(y_pred, y_true, axis=axis)
+
+def get_dice_focal_loss(y_pred, y_true, axis=AXIS):
+    return get_focal_loss(y_pred, y_true, axis=axis) + get_dice_loss(y_pred, y_true, axis=axis)
+
+def get_tversky_focal_loss(y_pred, y_true, axis=AXIS):
+    return get_focal_loss(y_pred, y_true, axis=axis) + get_tversky_loss(y_pred, y_true, axis=axis)
 
 
-def get_propotional_bce_loss(y_pred, y_true):
-    return get_bce_loss(y_pred, y_true) + get_propotional_loss(y_pred, y_true)
+def get_propotional_focal_loss(y_pred, y_true, axis=AXIS):
+    return get_focal_loss(y_pred, y_true, axis=axis) + get_propotional_loss(y_pred, y_true, axis=axis)
 
 
-def get_loss_fn(loss_select):
+def get_loss_fn(loss_select, axis=AXIS):
     if loss_select == "dice":
-        region_loss = get_dice_loss
+        region_loss = partial(get_dice_loss, axis=axis)
     elif loss_select == "tversky":
-        region_loss = get_tversky_loss
+        region_loss = partial(get_tversky_loss, axis=axis)
     elif loss_select == "propotional":
-        region_loss = get_propotional_loss
+        region_loss = partial(get_propotional_loss, axis=axis)
     elif loss_select == "dice_bce":
-        region_loss = get_dice_bce_loss
+        region_loss = partial(get_dice_bce_loss, axis=axis)
     elif loss_select == "tversky_bce":
-        region_loss = get_tversky_bce_loss
+        region_loss = partial(get_tversky_bce_loss, axis=axis)
     elif loss_select == "propotional_bce":
-        region_loss = get_propotional_bce_loss
+        region_loss = partial(get_propotional_bce_loss, axis=axis)
+    elif loss_select == "dice_focal":
+        region_loss = partial(get_dice_focal_loss, axis=axis)
+    elif loss_select == "tversky_focal":
+        region_loss = partial(get_tversky_focal_loss, axis=axis)
+    elif loss_select == "propotional_focal":
+        region_loss = partial(get_propotional_focal_loss, axis=axis)
 
     def final_loss_fn(y_pred, y_true):
         C = y_true.shape[1]
@@ -130,9 +146,9 @@ def get_loss_fn(loss_select):
     return final_loss_fn
 
 
-def get_dice_score(y_pred, y_true, mask_threshold=0.5):
+def get_dice_score(y_pred, y_true, axis=AXIS, mask_threshold=0.5):
     y_pred = (y_pred >= mask_threshold).float()
     y_true = (y_true >= mask_threshold).float()
-    dice_loss = get_dice_loss(y_pred, y_true, log=False, per_image=False)
+    dice_loss = get_dice_loss(y_pred, y_true, axis=axis, log=False, per_image=False)
     dice_score = 1 - dice_loss
     return dice_score
---
+++ src/model/inception_resnet_v2/diffusion/base_model_2d.py
@@ -0,0 +1,379 @@
+from torch import nn
+import numpy as np
+from .layers import LambdaLayer, ConcatBlock, ConvBlock2D, ConvBlock3D, DEFAULT_ACT
+from .inception_layers import Inception_Resnet_Block2D, Inception_Resnet_Block3D
+
+INPLACE = False
+
+
+def get_skip_connect_channel_list(block_size, mini=False):
+    if mini:
+        return np.array([block_size * 2, block_size * 4, block_size * 12])
+    else:
+        return np.array([block_size * 2, block_size * 4, block_size * 12,
+                        block_size * 68, block_size * 130])
+
+
+class InceptionResNetV2_2D(nn.Module):
+    def __init__(self, n_input_channels, block_size=16,
+                 padding='valid', norm="batch", act=DEFAULT_ACT, last_act=DEFAULT_ACT,
+                 last_channel_ratio=1, include_cbam=False, include_context=False,
+                 include_skip_connection_tensor=False):
+        super().__init__()
+        self.include_skip_connection_tensor = include_skip_connection_tensor
+        if padding == 'valid':
+            padding_3x3 = 0
+        elif padding == 'same':
+            padding_3x3 = 1
+
+        # Stem block
+        self.stem = nn.ModuleDict({
+            'stem_layer_1': ConvBlock2D(n_input_channels, block_size * 2, 3,
+                                        stride=2, padding=padding_3x3, norm=norm, act=act),
+            'stem_layer_2': ConvBlock2D(block_size * 2, block_size * 2, 3,
+                                        padding=padding_3x3, norm=norm, act=act),
+            'stem_layer_3': ConvBlock2D(block_size * 2, block_size * 4, 3, norm=norm, act=act),
+            'stem_layer_4': nn.MaxPool2d(3, stride=2, padding=padding_3x3),
+            'stem_layer_5': ConvBlock2D(block_size * 4, block_size * 5, 1, norm=norm, act=act),
+            'stem_layer_6': ConvBlock2D(block_size * 5, block_size * 12, 3,
+                                        padding=padding_3x3, norm=norm, act=act),
+            'stem_layer_7': nn.MaxPool2d(3, stride=2, padding=padding_3x3)
+        })
+        # Mixed 5b (Inception-A block): 35 x 35 x 320
+        mixed_5b_branch_0 = ConvBlock2D(block_size * 12, block_size * 6, 1,
+                                        norm=norm, act=act)
+        mixed_5b_branch_1 = nn.Sequential(
+            ConvBlock2D(block_size * 12, block_size *
+                        3, 1, norm=norm, act=act),
+            ConvBlock2D(block_size * 3, block_size * 4, 5, norm=norm, act=act)
+        )
+        mixed_5b_branch_2 = nn.Sequential(
+            ConvBlock2D(block_size * 12, block_size *
+                        4, 1, norm=norm, act=act),
+            ConvBlock2D(block_size * 4, block_size * 6, 3, norm=norm, act=act),
+            ConvBlock2D(block_size * 6, block_size * 6, 3, norm=norm, act=act)
+        )
+        mixed_5b_branch_pool = nn.Sequential(
+            nn.AvgPool2d(3, stride=1, padding=1),
+            ConvBlock2D(block_size * 12, block_size * 4, 1, norm=norm, act=act)
+        )
+        mixed_5b_branches = [mixed_5b_branch_0, mixed_5b_branch_1,
+                             mixed_5b_branch_2, mixed_5b_branch_pool]
+        self.mixed_5b = ConcatBlock(mixed_5b_branches)
+        # 10x block35 (Inception-ResNet-A block): 35 x 35 x 320
+        self.block_35 = nn.Sequential(*[
+            Inception_Resnet_Block2D(in_channels=block_size * 20, scale=0.17,
+                                     block_type="block35", norm=norm, act=act,
+                                     include_cbam=include_cbam,
+                                     include_context=include_context)
+            for _ in range(1, 11)
+        ])
+        # Mixed 6a (Reduction-A block): 17 x 17 x 1088
+        mixed_6a_branch_0 = ConvBlock2D(block_size * 20, block_size * 24, 3,
+                                        stride=2, padding=padding_3x3, norm=norm, act=act)
+        mixed_6a_branch_1 = nn.Sequential(
+            ConvBlock2D(block_size * 20, block_size * 16, 1,
+                        norm=norm, act=act),
+            ConvBlock2D(block_size * 16, block_size * 16, 3,
+                        norm=norm, act=act),
+            ConvBlock2D(block_size * 16, block_size * 24, 3,
+                        stride=2, padding=padding_3x3, norm=norm, act=act)
+        )
+        mixed_6a_branch_pool = nn.MaxPool2d(3, stride=2,
+                                            padding=padding_3x3)
+        mixed_6a_branches = [mixed_6a_branch_0, mixed_6a_branch_1,
+                             mixed_6a_branch_pool]
+        self.mixed_6a = ConcatBlock(mixed_6a_branches)
+        # 20x block17 (Inception-ResNet-B block): 17 x 17 x 1088
+        self.block_17 = nn.Sequential(*[
+            Inception_Resnet_Block2D(in_channels=block_size * 68, scale=0.1,
+                                     block_type="block17", norm=norm, act=act,
+                                     include_cbam=include_cbam,
+                                     include_context=(include_context and block_idx == 20))
+            for block_idx in range(1, 21)
+        ])
+        # Mixed 7a (Reduction-B block): 8 x 8 x 2080
+        mixed_7a_branch_0 = nn.Sequential(
+            ConvBlock2D(block_size * 68, block_size * 16, 1,
+                        norm=norm, act=act),
+            ConvBlock2D(block_size * 16, block_size *
+                        24, 3, stride=2, padding=padding_3x3,
+                        norm=norm, act=act)
+        )
+        mixed_7a_branch_1 = nn.Sequential(
+            ConvBlock2D(block_size * 68, block_size * 16, 1,
+                        norm=norm, act=act),
+            ConvBlock2D(block_size * 16, block_size *
+                        18, 3, stride=2, padding=padding_3x3,
+                        norm=norm, act=act)
+        )
+        mixed_7a_branch_2 = nn.Sequential(
+            ConvBlock2D(block_size * 68, block_size * 16, 1,
+                        norm=norm, act=act),
+            ConvBlock2D(block_size * 16, block_size * 18, 3,
+                        norm=norm, act=act),
+            ConvBlock2D(block_size * 18, block_size * 20, 3, stride=2,
+                        padding=padding_3x3, norm=norm, act=act)
+        )
+        mixed_7a_branch_pool = nn.MaxPool2d(3, stride=2,
+                                            padding=padding_3x3)
+        mixed_7a_branches = [mixed_7a_branch_0, mixed_7a_branch_1,
+                             mixed_7a_branch_2, mixed_7a_branch_pool]
+        self.mixed_7a = ConcatBlock(mixed_7a_branches)
+        # 10x block8 (Inception-ResNet-C block): 8 x 8 x 2080
+        self.block_8 = nn.Sequential(*[
+            Inception_Resnet_Block2D(in_channels=block_size * 130, scale=0.2,
+                                     block_type="block8", norm=norm, act=act,
+                                     include_cbam=include_cbam,
+                                     include_context=(include_context and block_idx == 10))
+            for block_idx in range(1, 11)
+        ])
+        # Final convolution block: 8 x 8 x 1536
+        self.final_conv = ConvBlock2D(block_size * 130, block_size * 96 * last_channel_ratio, 1,
+                                      norm=norm, act=last_act)
+
+    def forward(self, input_tensor):
+        skip_connect_index = 0
+        stem = input_tensor
+        for index, (layer_name, layer) in enumerate(self.stem.items()):
+            stem = layer(stem)
+            # layer_name in ["stem_layer_1", "stem_layer_4", "stem_layer_7"]
+            if self.include_skip_connection_tensor and (index in [0, 3, 6]):
+                setattr(self, f"skip_connect_tensor_{skip_connect_index}",
+                        stem)
+                skip_connect_index += 1
+        mixed_5b = self.mixed_5b(stem)
+        block_35 = self.block_35(mixed_5b)
+        mixed_6a = self.mixed_6a(block_35)
+        block_17 = self.block_17(mixed_6a)
+        mixed_7a = self.mixed_7a(block_17)
+        block_8 = self.block_8(mixed_7a)
+        output = self.final_conv(block_8)
+        if self.include_skip_connection_tensor:
+            setattr(self, f"skip_connect_tensor_{skip_connect_index}",
+                    mixed_6a)
+            setattr(self, f"skip_connect_tensor_{skip_connect_index + 1}",
+                    mixed_7a)
+        return output
+
+
+class MiniInceptionResNetV2_2D(nn.Module):
+    def __init__(self, n_input_channels, block_size=16,
+                 padding='valid', norm="batch", act=DEFAULT_ACT, last_act=DEFAULT_ACT, last_channel_ratio=1,
+                 include_cbam=False, include_context=False,
+                 include_skip_connection_tensor=False):
+        super().__init__()
+        self.include_skip_connection_tensor = include_skip_connection_tensor
+        if padding == 'valid':
+            padding_3x3 = 0
+        elif padding == 'same':
+            padding_3x3 = 1
+
+        # Stem block
+        self.stem = nn.ModuleDict({
+            'stem_layer_1': ConvBlock2D(n_input_channels, block_size * 2, 3, stride=2, padding=padding_3x3),
+            'stem_layer_2': ConvBlock2D(block_size * 2, block_size * 2, 3, padding=padding_3x3),
+            'stem_layer_3': ConvBlock2D(block_size * 2, block_size * 4, 3),
+            'stem_layer_4': nn.MaxPool2d(3, stride=2, padding=padding_3x3),
+            'stem_layer_5': ConvBlock2D(block_size * 4, block_size * 5, 1),
+            'stem_layer_6': ConvBlock2D(block_size * 5, block_size * 12, 3, padding=padding_3x3),
+            'stem_layer_7': nn.MaxPool2d(3, stride=2, padding=padding_3x3)
+        })
+        # Mixed 5b (Inception-A block): 35 x 35 x 320
+        mixed_5b_branch_0 = ConvBlock2D(block_size * 12, block_size * 6, 1)
+        mixed_5b_branch_1 = nn.Sequential(
+            ConvBlock2D(block_size * 12, block_size * 3, 1),
+            ConvBlock2D(block_size * 3, block_size * 4, 5)
+        )
+        mixed_5b_branch_2 = nn.Sequential(
+            ConvBlock2D(block_size * 12, block_size * 4, 1),
+            ConvBlock2D(block_size * 4, block_size * 6, 3),
+            ConvBlock2D(block_size * 6, block_size * 6, 3)
+        )
+        mixed_5b_branch_pool = nn.Sequential(
+            nn.AvgPool2d(3, stride=1, padding=1),
+            ConvBlock2D(block_size * 12, block_size * 4, 1)
+        )
+        mixed_5b_branches = [mixed_5b_branch_0, mixed_5b_branch_1,
+                             mixed_5b_branch_2, mixed_5b_branch_pool]
+        self.mixed_5b = ConcatBlock(mixed_5b_branches)
+        # 10x block35 (Inception-ResNet-A block): 35 x 35 x 320
+        self.block_35 = nn.Sequential(*[
+            Inception_Resnet_Block2D(in_channels=block_size * 20, scale=0.17,
+                                     block_type="block35",
+                                     include_cbam=include_cbam,
+                                     include_context=include_context)
+            for _ in range(1, 11)
+        ])
+        # Final convolution block: 8 x 8 x 1536
+        self.final_conv = ConvBlock2D(block_size * 20, block_size * 32 * last_channel_ratio, 1,
+                                      act=last_act)
+
+    def forward(self, input_tensor):
+        skip_connect_index = 0
+        stem = input_tensor
+        for index, (layer_name, layer) in enumerate(self.stem.items()):
+            stem = layer(stem)
+            # layer_name in ["stem_layer_1", "stem_layer_4", "stem_layer_7"]
+            if self.include_skip_connection_tensor and (index in [0, 3, 6]):
+                setattr(self, f"skip_connect_tensor_{skip_connect_index}",
+                        stem)
+                skip_connect_index += 1
+        mixed_5b = self.mixed_5b(stem)
+        block_35 = self.block_35(mixed_5b)
+        output = self.final_conv(block_35)
+        return output
+
+
+class InceptionResNetV2_3D(nn.Module):
+    def __init__(self, n_input_channels, block_size=16,
+                 padding='valid', norm="batch", act=DEFAULT_ACT, last_act=DEFAULT_ACT,
+                 z_channel_preserve=True, include_context=False,
+                 include_skip_connection_tensor=False):
+        super().__init__()
+        self.include_skip_connection_tensor = include_skip_connection_tensor
+        if padding == 'valid':
+            padding_3x3 = 0
+        elif padding == 'same':
+            padding_3x3 = 1
+
+        if z_channel_preserve:
+            z_stride = [1, 2, 2]
+        else:
+            z_stride = 2
+        # Stem block
+        self.stem = nn.ModuleDict({
+            'stem_layer_1': ConvBlock3D(n_input_channels, block_size * 2, 3,
+                                        stride=z_stride, padding=padding_3x3, norm=norm, act=act),
+            'stem_layer_2': ConvBlock3D(block_size * 2, block_size * 2, 3,
+                                        padding=padding_3x3, norm=norm, act=act),
+            'stem_layer_3': ConvBlock3D(block_size * 2, block_size * 4, 3, norm=norm, act=act),
+            'stem_layer_4': nn.MaxPool3d(3, stride=z_stride, padding=padding_3x3),
+            'stem_layer_5': ConvBlock3D(block_size * 4, block_size * 5, 1, norm=norm, act=act),
+            'stem_layer_6': ConvBlock3D(block_size * 5, block_size * 12, 3,
+                                        padding=padding_3x3, norm=norm, act=act),
+            'stem_layer_7': nn.MaxPool3d(3, stride=z_stride, padding=padding_3x3)
+        })
+        # Mixed 5b (Inception-A block): 35 x 35 x 320
+        mixed_5b_branch_0 = ConvBlock3D(block_size * 12, block_size * 6, 1,
+                                        norm=norm, act=act)
+        mixed_5b_branch_1 = nn.Sequential(
+            ConvBlock3D(block_size * 12, block_size * 3, 1,
+                        norm=norm, act=act),
+            ConvBlock3D(block_size * 3, block_size * 4, 5,
+                        norm=norm, act=act)
+        )
+        mixed_5b_branch_2 = nn.Sequential(
+            ConvBlock3D(block_size * 12, block_size * 4, 1,
+                        norm=norm, act=act),
+            ConvBlock3D(block_size * 4, block_size * 6, 3,
+                        norm=norm, act=act),
+            ConvBlock3D(block_size * 6, block_size * 6, 3,
+                        norm=norm, act=act)
+        )
+        mixed_5b_branch_pool = nn.Sequential(
+            nn.AvgPool3d(3, stride=1, padding=1),
+            ConvBlock3D(block_size * 12, block_size * 4, 1,
+                        norm=norm, act=act)
+        )
+        mixed_5b_branches = [mixed_5b_branch_0, mixed_5b_branch_1,
+                             mixed_5b_branch_2, mixed_5b_branch_pool]
+        self.mixed_5b = ConcatBlock(mixed_5b_branches)
+        # 10x block35 (Inception-ResNet-A block): 35 x 35 x 320
+        self.block_35 = nn.Sequential(*[
+            Inception_Resnet_Block3D(in_channels=block_size * 20, scale=0.17,
+                                     block_type="block35", norm=norm, act=act,
+                                     block_size=block_size,
+                                     include_context=include_context)
+            for _ in range(1, 11)
+        ])
+        # Mixed 6a (Reduction-A block): 17 x 17 x 1088
+        mixed_6a_branch_0 = ConvBlock3D(block_size * 20, block_size * 24, 3,
+                                        stride=z_stride, padding=padding_3x3,
+                                        norm=norm, act=act)
+        mixed_6a_branch_1 = nn.Sequential(
+            ConvBlock3D(block_size * 20, block_size * 16, 1,
+                        norm=norm, act=act),
+            ConvBlock3D(block_size * 16, block_size * 16, 3,
+                        norm=norm, act=act),
+            ConvBlock3D(block_size * 16, block_size * 24, 3,
+                        stride=z_stride, padding=padding_3x3,
+                        norm=norm, act=act)
+        )
+        mixed_6a_branch_pool = nn.MaxPool3d(3, stride=z_stride,
+                                            padding=padding_3x3)
+        mixed_6a_branches = [mixed_6a_branch_0, mixed_6a_branch_1,
+                             mixed_6a_branch_pool]
+        self.mixed_6a = ConcatBlock(mixed_6a_branches)
+        # 20x block17 (Inception-ResNet-B block): 17 x 17 x 1088
+        self.block_17 = nn.Sequential(*[
+            Inception_Resnet_Block3D(in_channels=block_size * 68, scale=0.1,
+                                     block_type="block17", norm=norm, act=act,
+                                     block_size=block_size,
+                                     include_context=(include_context and block_idx == 20))
+            for block_idx in range(1, 21)
+        ])
+        # Mixed 7a (Reduction-B block): 8 x 8 x 2080
+        mixed_7a_branch_0 = nn.Sequential(
+            ConvBlock3D(block_size * 68, block_size * 16, 1,
+                        norm=norm, act=act),
+            ConvBlock3D(block_size * 16, block_size * 24, 3,
+                        stride=z_stride, padding=padding_3x3,
+                        norm=norm, act=act)
+        )
+        mixed_7a_branch_1 = nn.Sequential(
+            ConvBlock3D(block_size * 68, block_size * 16, 1,
+                        norm=norm, act=act),
+            ConvBlock3D(block_size * 16, block_size * 18, 3,
+                        stride=z_stride, padding=padding_3x3,
+                        norm=norm, act=act)
+        )
+        mixed_7a_branch_2 = nn.Sequential(
+            ConvBlock3D(block_size * 68, block_size * 16, 1,
+                        norm=norm, act=act),
+            ConvBlock3D(block_size * 16, block_size * 18, 3,
+                        norm=norm, act=act),
+            ConvBlock3D(block_size * 18, block_size * 20, 3,
+                        stride=z_stride, padding=padding_3x3,
+                        norm=norm, act=act)
+        )
+        mixed_7a_branch_pool = nn.MaxPool3d(3, stride=z_stride,
+                                            padding=padding_3x3)
+        mixed_7a_branches = [mixed_7a_branch_0, mixed_7a_branch_1,
+                             mixed_7a_branch_2, mixed_7a_branch_pool]
+        self.mixed_7a = ConcatBlock(mixed_7a_branches)
+        # 10x block8 (Inception-ResNet-C block): 8 x 8 x 2080
+        self.block_8 = nn.Sequential(*[
+            Inception_Resnet_Block3D(in_channels=block_size * 130, scale=0.2,
+                                     block_type="block8", norm=norm, act=act,
+                                     block_size=block_size,
+                                     include_context=(include_context and block_idx == 10))
+            for block_idx in range(1, 11)
+        ])
+        # Final convolution block: 8 x 8 x 1536
+        self.final_conv = ConvBlock3D(block_size * 130, block_size * 96, 1,
+                                      norm=norm, act=last_act)
+
+    def forward(self, input_tensor):
+        skip_connect_index = 0
+        stem = input_tensor
+        for index, (layer_name, layer) in enumerate(self.stem.items()):
+            stem = layer(stem)
+            # layer_name in ["stem_layer_1", "stem_layer_4", "stem_layer_7"]
+            if self.include_skip_connection_tensor and (index in [0, 3, 6]):
+                setattr(self, f"skip_connect_tensor_{skip_connect_index}",
+                        stem)
+                skip_connect_index += 1
+        mixed_5b = self.mixed_5b(stem)
+        block_35 = self.block_35(mixed_5b)
+        mixed_6a = self.mixed_6a(block_35)
+        block_17 = self.block_17(mixed_6a)
+        mixed_7a = self.mixed_7a(block_17)
+        block_8 = self.block_8(mixed_7a)
+        output = self.final_conv(block_8)
+        if self.include_skip_connection_tensor:
+            setattr(self, f"skip_connect_tensor_{skip_connect_index}",
+                    mixed_6a)
+            setattr(self, f"skip_connect_tensor_{skip_connect_index + 1}",
+                    mixed_7a)
+        return output
