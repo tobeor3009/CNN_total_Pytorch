810c91ac3a18c0870960586380655f20ff33d3e3
tobeor3009 <tobeor3009@gmail.com>
2024-03-28 16:14:38 +0900

    daily commit

--- src/model/med_seg_diff/model_class.py
+++ src/model/med_seg_diff/model_class.py
@@ -70,7 +70,6 @@ class MedSegDiff(nn.Module):
         self.mask_channels = self.model.mask_channels
         self.self_condition = self.model.self_condition
         self.image_size = self.model.image_size
-        self.use_class_embed = self.model.use_class_embed
         self.objective = objective
 
         assert objective in {'pred_noise', 'pred_x0', 'pred_v'}, 'objective must be either pred_noise (predict noise) or pred_x0 (predict image start) or pred_v (predict v [v-parameterization as defined in appendix D of progressive distillation paper, used in imagen-video successfully])'
@@ -267,10 +266,7 @@ class MedSegDiff(nn.Module):
     def sample(self, cond_img, class_label):
         batch_size, device, dtype = cond_img.shape[0], self.device, self.dtype
         cond_img = cond_img.to(device=device, dtype=dtype)
-        if self.use_class_embed:
-            class_label = class_label.to(device=device, dtype=torch.long)
-        else:
-            class_label = class_label.to(device=device, dtype=dtype)
+        class_label = class_label.to(device=device, dtype=torch.long)
         image_size, mask_channels = self.image_size, self.mask_channels
         sample_fn = self.p_sample_loop if not self.is_ddim_sampling else self.ddim_sample
         return sample_fn((batch_size, mask_channels, image_size, image_size), cond_img, class_label).float()
@@ -330,10 +326,7 @@ class MedSegDiff(nn.Module):
         device, dtype = self.device, self.dtype
         img = img.to(device=device, dtype=dtype)
         cond_img = cond_img.to(device)
-        if self.use_class_embed:
-            class_label = class_label.to(device=device, dtype=torch.long)
-        else:
-            class_label = class_label.to(device=device, dtype=dtype)
+        class_label = class_label.to(device=device, dtype=torch.long)
 
         b, c, h, w, device, img_size, img_channels, mask_channels = *img.shape, img.device, self.image_size, self.input_img_channels, self.mask_channels
 
