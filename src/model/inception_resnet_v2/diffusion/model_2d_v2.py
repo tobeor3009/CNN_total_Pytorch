89af98dab937c7b0eef2e7981313308164bbdb6f
tobeor3009 <tobeor3009@gmail.com>
2024-03-28 17:09:25 +0900

    bug fix diffusion

--- src/model/inception_resnet_v2/diffusion/model_2d_v2.py
+++ src/model/inception_resnet_v2/diffusion/model_2d_v2.py
@@ -42,13 +42,14 @@ class InceptionResNetV2_UNet(nn.Module):
             block_depth_list = block_depth_info
         assert isinstance(num_class_embeds, int) and num_class_embeds > 0, "you need to assign positive int to num_class_embeds"
         
-        if self_condition:
-            in_channel *= 2
+
         # for compability with Medsegdiff
         self.image_size = img_size
         self.input_img_channels = cond_channel
         self.mask_channels = in_channel
         self.self_condition = self_condition
+        if self.self_condition:
+            in_channel *= 2
         ##################################
         emb_dim = block_size * 16
         emb_dim_list = [emb_dim, emb_dim]
