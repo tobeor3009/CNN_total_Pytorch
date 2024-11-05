bde0648c8251e308b07fea741b38b07ab7354bdf
tobeor3009 <tobeor3009@gmail.com>
2024-11-06 01:46:13 +0900

    daily commit

--- src/model/swin_transformer/model_2d/multi_task_v4.py
+++ src/model/swin_transformer/model_2d/multi_task_v4.py
@@ -334,14 +334,17 @@ class SwinMultitask(nn.Module):
         h, w = self.feature_hw
         common_kwarg_dict = self.get_layer_config_dict(self.feature_dim, self.feature_hw, i_layer)
         validity_layer_1 = BasicLayerV2(**common_kwarg_dict)
+        validity_mlp_1 = Mlp(self.feature_dim, hidden_features=None,
+                           out_features=self.feature_dim, act_layer=get_act(self.model_act_layer))
         validity_layer_2 = BasicLayerV2(**common_kwarg_dict)
-        validity_mlp = Mlp(self.feature_dim, hidden_features=self.feature_dim // 2,
+        validity_mlp_2 = Mlp(self.feature_dim, hidden_features=self.feature_dim // 2,
                            out_features=validity_shape[0], act_layer=get_act(self.model_act_layer))
         validity_avg_pool = nn.AdaptiveAvgPool2d(validity_shape[1:])
         validity_head = nn.Sequential(
             validity_layer_1,
+            validity_mlp_1,
             validity_layer_2,
-            validity_mlp,
+            validity_mlp_2,
             Rearrange('b (h w) c -> b c h w', h=h, w=w),
             validity_avg_pool,
             get_act(validity_act)
