038830ed6b69e001ef3e500ab13ca0fe3a76b2c7
tobeor3009 <tobeor3009@gmail.com>
2024-11-20 18:28:11 +0900

    update new version of diffusion

--- src/model/inception_resnet_v2/diffusion/diffusion_layer.py
+++ src/model/inception_resnet_v2/diffusion/diffusion_layer.py
@@ -11,8 +11,15 @@ import torch.nn.functional as F
 from torch.utils.checkpoint import checkpoint
 from einops import rearrange, repeat
 from ..common_module.cbam import CBAM
-from ..common_module.layers import get_act, get_norm, DEFAULT_ACT
+from ..common_module.layers import get_act, get_norm, DEFAULT_ACT, INPLACE
 from ..common_module.layers import ConcatBlock
+
+conv_transpose_kwarg_dict = {
+    "kernel_size":3,
+    "padding":1,
+    "output_padding":1,
+    "bias":True
+}
 
 def exists(x):
     return x is not None
@@ -263,11 +270,12 @@ class LearnedSinusoidalPosEmb(nn.Module):
         freqs = x * rearrange(self.weights, 'd -> 1 d') * 2 * math.pi
         fouriered = torch.cat((freqs.sin(), freqs.cos()), dim = -1)
         fouriered = torch.cat((x, fouriered), dim = -1)
-        return fouriered    
+        return fouriered
+
 class BaseBlock2D(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size,
                  stride=1, padding='same',
-                 norm=nn.LayerNorm, groups=1, act=DEFAULT_ACT, bias=False):
+                 norm=nn.LayerNorm, groups=1, act=DEFAULT_ACT, bias=False, dropout_proba=0.0):
         super().__init__()
 
         self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels,
@@ -278,9 +286,10 @@ class BaseBlock2D(nn.Module):
         else:
             self.norm_layer = nn.Identity()
         self.act_layer = get_act(act)
-
+        self.dropout_layer = nn.Dropout2d(p=dropout_proba, inplace=INPLACE)
     def forward(self, x, scale_shift_list=None):
         conv = self.conv(x)
+        conv = self.dropout_layer(conv)
         norm = self.norm_layer(conv)
         if exists(scale_shift_list):
             for scale_shift in scale_shift_list:
@@ -293,7 +302,7 @@ class BaseBlock2D(nn.Module):
 class ResNetBlock2D(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size,
                  stride=1, padding='same',
-                 norm="batch", groups=1, act=DEFAULT_ACT, bias=False,
+                 norm="batch", groups=1, act=DEFAULT_ACT, bias=False, dropout_proba=0.0,
                  emb_dim_list=[], emb_type_list=[], attn_info=None, use_checkpoint=False):
         super().__init__()
 
@@ -308,16 +317,16 @@ class ResNetBlock2D(nn.Module):
                                         )
             elif emb_type == "2d":
                 emb_block = BaseBlock2D(emb_dim, out_channels * 2, kernel_size,
-                                        1, padding, norm, groups, act, bias)
+                                        1, padding, norm, groups, act, bias, dropout_proba=0.0)
             else:
                 raise Exception("emb_type must be seq or 2d")
             emb_block_list.append(emb_block)
         self.emb_block_list = nn.ModuleList(emb_block_list)
 
         self.block_1 = BaseBlock2D(in_channels, out_channels, kernel_size,
-                                    stride, padding, norm, groups, act, bias)
+                                    stride, padding, norm, groups, act, bias, dropout_proba=dropout_proba)
         self.block_2 = BaseBlock2D(out_channels, out_channels, kernel_size,
-                                    1, padding, norm, groups, act, bias)
+                                    1, padding, norm, groups, act, bias, dropout_proba=dropout_proba)
 
         if in_channels != out_channels or stride != 1:
             self.residiual_conv = nn.Conv2d(in_channels, out_channels, 1, stride, bias=False)
@@ -359,7 +368,7 @@ class ResNetBlock2D(nn.Module):
 class ConvBlock2D(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size,
                  stride=1, padding='same',
-                 norm="batch", groups=1, act=DEFAULT_ACT, bias=False,
+                 norm="batch", groups=1, act=DEFAULT_ACT, bias=False, dropout_proba=0.0,
                  emb_dim_list=[], emb_type_list=[], attn_info=None, use_checkpoint=False):
         super().__init__()
 
@@ -374,19 +383,21 @@ class ConvBlock2D(nn.Module):
         for emb_dim, emb_type in zip(emb_dim_list, emb_type_list):
             if emb_type == "seq":
                 emb_block = nn.Sequential(
-                                            nn.SiLU(),
+                                            get_act(act),
                                             nn.Linear(emb_dim, out_channels * 2)
                                         )
             elif emb_type == "2d":
                 emb_block = BaseBlock2D(emb_dim, out_channels * 2, kernel_size,
-                                        1, padding, norm, groups, act, bias)
+                                        1, padding, norm, groups, act, bias,
+                                        dropout_proba=0.0)
             else:
                 raise Exception("emb_type must be seq or 2d")
             emb_block_list.append(emb_block)
         self.emb_block_list = nn.ModuleList(emb_block_list)
 
         self.block_1 = BaseBlock2D(in_channels, out_channels, kernel_size,
-                                    stride, padding, norm, groups, act, bias)
+                                    stride, padding, norm, groups, act, bias, 
+                                    dropout_proba=dropout_proba)
         if attn_info is None:
             self.attn = nn.Identity()
         elif attn_info["full_attn"] is None:
@@ -418,83 +429,63 @@ class ConvBlock2D(nn.Module):
 
 class Inception_Resnet_Block2D(nn.Module):
     def __init__(self, in_channels, scale, block_type, block_size=16,
-                 include_cbam=True, norm="batch", act=DEFAULT_ACT, 
+                 include_cbam=True, norm="batch", act=DEFAULT_ACT, dropout_proba=0.0,
                  emb_dim_list=None, attn_info=None, use_checkpoint=False):
         super().__init__()
         assert emb_dim_list is not None, f"You need to set emb_dim_list. current emb_dim_list: {emb_dim_list}"
         self.include_cbam = include_cbam
         self.scale = scale
+        common_kwarg_dict = {
+            "norm": norm,
+            "act": act,
+            "emb_dim_list": emb_dim_list,
+            "attn_info": attn_info,
+            "use_checkpoint": use_checkpoint
+        }
+
         if block_type == 'block35':
             branch_0 = ConvBlock2D(in_channels, block_size * 2, 1,
-                                   norm=norm, act=act,
-                                   emb_dim_list=emb_dim_list, attn_info=attn_info,
-                                   use_checkpoint=use_checkpoint)
+                                   **common_kwarg_dict)
             branch_1 = MultiInputSequential(
                 ConvBlock2D(in_channels, block_size * 2, 1,
-                            norm=norm, act=act,
-                            emb_dim_list=emb_dim_list, attn_info=attn_info, 
-                            use_checkpoint=use_checkpoint),
+                            **common_kwarg_dict),
                 ConvBlock2D(block_size * 2, block_size * 2, 3,
-                            norm=norm, act=act,
-                            emb_dim_list=emb_dim_list, attn_info=attn_info,
-                            use_checkpoint=use_checkpoint)
+                            **common_kwarg_dict)
             )
             branch_2 = MultiInputSequential(
                 ConvBlock2D(in_channels, block_size * 2, 1,
-                            norm=norm, act=act, 
-                            emb_dim_list=emb_dim_list, attn_info=attn_info,
-                            use_checkpoint=use_checkpoint),
+                            **common_kwarg_dict),
                 ConvBlock2D(block_size * 2, block_size * 3, 3,
-                            norm=norm, act=act,
-                            emb_dim_list=emb_dim_list, attn_info=attn_info,
-                            use_checkpoint=use_checkpoint),
+                            **common_kwarg_dict),
                 ConvBlock2D(block_size * 3, block_size * 4, 3,
-                            norm=norm, act=act,
-                            emb_dim_list=emb_dim_list, attn_info=attn_info,
-                            use_checkpoint=use_checkpoint)
+                            **common_kwarg_dict)
             )
             mixed_channel = block_size * 8
             # block_size * (2 + 2 + 4) => block_size * 10
             branches = [branch_0, branch_1, branch_2]
         elif block_type == 'block17':
             branch_0 = ConvBlock2D(in_channels, block_size * 12, 1,
-                                   norm=norm, act=act,
-                                   emb_dim_list=emb_dim_list, attn_info=attn_info,
-                                   use_checkpoint=use_checkpoint)
+                                   **common_kwarg_dict)
             branch_1 = MultiInputSequential(
                 ConvBlock2D(in_channels, block_size * 8, 1,
-                            norm=norm, act=act,
-                            emb_dim_list=emb_dim_list, attn_info=attn_info,
-                            use_checkpoint=use_checkpoint),
+                            **common_kwarg_dict),
                 ConvBlock2D(block_size * 8, block_size * 10, [1, 7],
-                            norm=norm, act=act,
-                            emb_dim_list=emb_dim_list, attn_info=attn_info,
-                            use_checkpoint=use_checkpoint),
+                            **common_kwarg_dict),
                 ConvBlock2D(block_size * 10, block_size * 12, [7, 1],
-                            norm=norm, act=act,
-                            emb_dim_list=emb_dim_list, attn_info=attn_info,
-                            use_checkpoint=use_checkpoint)
+                            **common_kwarg_dict)
             )
             mixed_channel = block_size * 24
             branches = [branch_0, branch_1]
         elif block_type == 'block8':
             branch_0 = ConvBlock2D(in_channels, block_size * 12, 1,
-                                   norm=norm, act=act,
-                                   emb_dim_list=emb_dim_list, attn_info=attn_info,
-                                   use_checkpoint=use_checkpoint)
+                                   **common_kwarg_dict)
             branch_1 = MultiInputSequential(
                 ConvBlock2D(in_channels, block_size * 12, 1,
-                            norm=norm, act=act,
-                            emb_dim_list=emb_dim_list, attn_info=attn_info,
-                            use_checkpoint=use_checkpoint),
+                            **common_kwarg_dict),
                 ConvBlock2D(block_size * 12, block_size * 14, [1, 3],
-                            norm=norm, act=act,
-                            emb_dim_list=emb_dim_list, attn_info=attn_info,
-                            use_checkpoint=use_checkpoint),
+                            **common_kwarg_dict),
                 ConvBlock2D(block_size * 14, block_size * 16, [3, 1],
-                            norm=norm, act=act,
-                            emb_dim_list=emb_dim_list, attn_info=attn_info,
-                            use_checkpoint=use_checkpoint)
+                            **common_kwarg_dict)
             )
             mixed_channel = block_size * 28
             branches = [branch_0, branch_1]
@@ -574,11 +565,67 @@ class MultiDecoder2D(nn.Module):
         out = torch.cat([pixel_shuffle, upsample], dim=1)
         out = self.concat_conv(out, *args)
         return out
-    
+
+class MultiDecoder2D_V2(nn.Module):
+    def __init__(self, in_channels, skip_channels, out_channels,
+                 norm="layer", act=DEFAULT_ACT, kernel_size=2,
+                 emb_dim_list=None, emb_type_list=None, attn_info=None, use_checkpoint=False):
+        super().__init__()
+
+        self.use_checkpoint = use_checkpoint
+        conv_common_kwarg_dict = {
+            "kernel_size": 3, "stride": 1, "padding": 1,
+            "norm": norm, "act": act,
+            "emb_dim_list": emb_dim_list,
+            "emb_type_list": emb_type_list,
+            "attn_info": attn_info,
+            "use_checkpoint": use_checkpoint
+        }
+        if isinstance(kernel_size, int):
+            kernel_size = (kernel_size, kernel_size)
+        conv_before_pixel_shuffle = nn.Conv2d(in_channels=in_channels,
+                                              out_channels=in_channels *
+                                              np.prod(kernel_size),
+                                              kernel_size=1)
+        pixel_shuffle_layer = nn.PixelShuffle(upscale_factor=(kernel_size
+                                                              if isinstance(kernel_size, int)
+                                                              else kernel_size[0]))
+        conv_after_pixel_shuffle = nn.Conv2d(in_channels=in_channels,
+                                             out_channels=out_channels,
+                                             kernel_size=1)
+        self.pixel_shuffle = nn.Sequential(
+            conv_before_pixel_shuffle,
+            pixel_shuffle_layer,
+            conv_after_pixel_shuffle
+        )
+        self.conv_transpose = nn.ConvTranspose2d(in_channels, out_channels,
+                                                stride=kernel_size,
+                                                **conv_transpose_kwarg_dict)
+        self.concat_conv = ConvBlock2D(in_channels=out_channels * 2,
+                                       out_channels=out_channels, **conv_common_kwarg_dict)
+        self.skip_conv = ConvBlock2D(in_channels=out_channels + skip_channels,
+                                       out_channels=out_channels, **conv_common_kwarg_dict)
+    def forward(self, x, skip, *args):
+        if self.use_checkpoint:
+            return checkpoint(self._forward_impl, x, skip, *args,
+                              use_reentrant=False)
+        else:
+            return self._forward_impl(x, skip, *args)
+        
+    def _forward_impl(self, x, skip, *args):
+        pixel_shuffle = self.pixel_shuffle(x)
+        conv_transpose = self.conv_transpose(x)
+
+        out = torch.cat([pixel_shuffle, conv_transpose], dim=1)
+        out = self.concat_conv(out, *args)
+        out = torch.cat([out, skip], dim=1)
+        out = self.skip_conv(out)
+        return out
+
 class Output2D(nn.Module):
     def __init__(self, in_channels, out_channels, act=None):
         super().__init__()
-        conv_out_channels = in_channels // 2
+        conv_out_channels = in_channels
         self.conv_5x5 = nn.Conv2d(in_channels=in_channels,
                                   out_channels=conv_out_channels,
                                   kernel_size=5, padding=2)
