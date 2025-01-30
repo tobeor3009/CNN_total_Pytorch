fix formula and add multi task model

src/model/inception_resnet_v2/diffusion/diff_ae/diffusion_execute.py
@@ -2,17 +2,42 @@ import math
 import torch
 from torch import nn
 from torch.amp import autocast
-from torch.nn import functional as F 
+from torch.nn import functional as F
+from torch.utils.data import Dataset
 from .resample import UniformSampler
 from .diffusion_sampler import GaussianSampler
 from .renderer import render_uncondition, render_condition
 from src.model.train_util.common import _z_normalize, feature_z_normalize, z_normalize
+from tqdm import tqdm
 
 def identity_cat_fn(x, cond):
     return x
 
 def identity_split_fn(x_cated):
     return x_cated, x_cated
+
+class TargetStepDataset(Dataset):
+    def __init__(self, dataset, batch_size, target_stepsize):
+        self.dataset = dataset
+        self.dataset_real_len = len(dataset)
+        self.dataset_len = target_stepsize * batch_size
+        assert self.dataset_real_len >= len(dataset), "target_stepsize * batch_size is smaller than dataset_real_len"
+ 
+    def __len__(self):
+        return self.dataset_len
+
+    def __getitem__(self, idx):
+        idx = idx % self.dataset_real_len
+        return self.dataset[idx]
+class LatentDataset(Dataset):
+    def __init__(self, batch_cond):
+        self.batch_cond = batch_cond
+
+    def __len__(self):
+        return len(self.batch_cond)
+        
+    def __getitem__(self, idx):
+        return self.batch_cond[idx]
 
 
 class AutoEncoder(nn.Module):
@@ -112,6 +137,46 @@ class AutoEncoder(nn.Module):
         # 표준 정규분포 정의 (평균=0, 표준편차=1)
         # 99% 신뢰구간에 해당하는 누적 확률값 (0.5%와 99.5%)
         self.register_buffer('x_T', self.get_noise(sample_size))
+        
+    def compute_cond_mean_std(self, train_dataloader, encode_process_fn=None, device=None, cond_save_path=None):
+        device = device or torch.device("cuda")
+        
+        with torch.no_grad():
+            batch_idx = 0
+            cond_list = [None for _ in range(len(train_dataloader.dataset))]
+            for batch in tqdm(train_dataloader, total=len(train_dataloader)):
+                if encode_process_fn is not None:
+                    cond = encode_process_fn(batch, self.encode, device)
+                else:
+                    batch = batch.to(device)
+                    cond = self.encode(batch)
+                cond = cond.cpu()
+                for part_cond in cond:
+                    cond_list[batch_idx] = part_cond
+                    batch_idx += 1
+
+        batch_cond = torch.stack(cond_list, dim=0)
+        cond_mean = batch_cond.mean(dim=0, keepdim=True)
+        cond_std = batch_cond.std(dim=0, keepdim=True)
+
+        if cond_save_path is not None:
+            torch.save({
+                "batch_cond": batch_cond,
+                "cond_mean": cond_mean,
+                "cond_std": cond_std
+            }, cond_save_path)
+
+        return batch_cond, cond_mean, cond_std
+
+    def load_cond_into(self, cond_file_path, set_buffer=False):
+        cond_dict = torch.load(cond_file_path)
+        batch_cond = cond_dict["batch_cond"]
+        cond_mean = cond_dict["cond_mean"]
+        cond_std = cond_dict["cond_std"]
+        if set_buffer:
+            self.register_buffer("cond_mean", cond_mean)
+            self.register_buffer("cond_std", cond_std)
+        return LatentDataset(batch_cond)
 
     def get_noise(self, batch_size, noise_channel=None):
         noise_channel = noise_channel or self.in_channel
@@ -134,19 +199,66 @@ class AutoEncoder(nn.Module):
         else:
             sampler = self.get_sampler(T=self.T, T_eval=T)
             latent_sampler = self.get_latent_sampler(T=self.T, T_eval=T_latent)
+
+        if self.latent_znormalize:
+            assert (self.cond_mean is not None) and (self.cond_std is not None), "get cond_mean and cond_std first by compute_cond_mean_std class fn"
+            cond_mean = self.cond_mean
+            cond_std = self.cond_std
+        else:
+            cond_mean = None
+            cond_std = None
         pred_img = render_uncondition(
             self.diffusion_model,
             noise,
             sampler=sampler,
             latent_sampler=latent_sampler,
             latent_clip_sample=self.latent_clip_sample,
-            latent_znormalize=self.latent_znormalize,
+            cond_mean=cond_mean,
+            cond_std=cond_std,
             train_mode=self.train_mode,
-            clip_denoised=clip_denoised
+            clip_denoised=clip_denoised,
+            image_mask_cat_fn=self.image_mask_cat_fn,
+            image_mask_split_fn=self.image_mask_split_fn
         )
         pred_img = (pred_img + 1) / 2
         return pred_img
 
+    def sample_segmentation(self, image, device, T=None, T_latent=None, clip_denoised=True):
+        image = image.to(device)
+        noise = self.get_noise(len(image)).to(device=device)
+        _, noise = self.image_mask_split_fn(noise)
+        image_noise = self.image_mask_cat_fn(image, noise)
+        if T is None:
+            sampler = self.eval_sampler
+            latent_sampler = self.latent_sampler
+        else:
+            sampler = self.get_sampler(T=self.T, T_eval=T)
+            latent_sampler = self.get_latent_sampler(T=self.T, T_eval=T_latent)
+
+        if self.latent_znormalize:
+            assert (self.cond_mean is not None) and (self.cond_std is not None), "get cond_mean and cond_std first by compute_cond_mean_std class fn"
+            cond_mean = self.cond_mean
+            cond_std = self.cond_std
+        else:
+            cond_mean = None
+            cond_std = None
+
+        pred_img = render_uncondition(
+            self.diffusion_model,
+            image_noise,
+            sampler=sampler,
+            latent_sampler=latent_sampler,
+            latent_clip_sample=self.latent_clip_sample,
+            cond_mean=cond_mean,
+            cond_std=cond_std,
+            train_mode=self.train_mode,
+            clip_denoised=clip_denoised,
+            image_mask_cat_fn=self.image_mask_cat_fn,
+            image_mask_split_fn=self.image_mask_split_fn
+        )
+        pred_img = (pred_img + 1) / 2
+        return pred_img
+    
     def render(self, noise, cond=None, T=None, clip_denoised=True):
         if T is None:
             sampler = self.eval_sampler
@@ -165,7 +277,6 @@ class AutoEncoder(nn.Module):
             pred_img = render_uncondition(self.diffusion_model,
                                           noise, sampler, latent_sampler=None,
                                           latent_clip_sample=self.latent_clip_sample,
-                                          latent_znormalize=self.latent_znormalize,
                                           train_mode=self.train_mode,
                                           clip_denoised=clip_denoised,
                                           image_mask_cat_fn=self.image_mask_cat_fn,
@@ -205,7 +316,7 @@ class AutoEncoder(nn.Module):
 
 
     def forward(self, x_start=None, encoded_feature=None, cond_start=None):
-        if self.is_segmentation:
+        if self.is_segmentation and (self.train_mode != "latent_net"):
             return self._get_loss_segmentation(x_start=x_start, cond_start=cond_start)
         else:
             return self._get_loss(x_start=x_start, encoded_feature=encoded_feature, cond_start=cond_start)
@@ -238,12 +349,9 @@ class AutoEncoder(nn.Module):
                                                             model_kwargs={'cond': encoded_feature})
 
         elif self.train_mode == "latent_net":
-            if encoded_feature is None:
-                with torch.no_grad():
-                    encoded_feature = self.encode(x_start)
-            t, weight = self.T_sampler.sample(len(encoded_feature), torch_device)
+            t, weight = self.T_sampler.sample(len(x_start), torch_device)
             result_dict = self.latent_sampler.training_losses(model=self.diffusion_model.latent_net,
-                                                                x_start=encoded_feature, t=t)
+                                                                x_start=x_start, t=t)
             
         elif self.train_mode == "autoencoder_latent_net":
     

src/model/inception_resnet_v2/diffusion/diff_ae/diffusion_layer.py
@@ -11,7 +11,7 @@ import torch.nn.functional as F
 from torch.utils.checkpoint import checkpoint
 from einops import rearrange, repeat
 from src.model.inception_resnet_v2.common_module.cbam import CBAM
-from src.model.inception_resnet_v2.common_module.layers import get_act, get_norm, DEFAULT_ACT, INPLACE
+from src.model.inception_resnet_v2.common_module.layers import get_act, DEFAULT_ACT, INPLACE
 from src.model.inception_resnet_v2.common_module.layers import ConcatBlock
 from .nn import zero_module, conv_nd
 
@@ -344,8 +344,10 @@ class AttentionBlock(nn.Module):
         num_head_channels=-1,
         use_checkpoint=False,
         use_new_attention_order=False,
+        norm_layer="group"
     ):  
         super().__init__()
+        assert norm_layer in ["rms", "group"]
         self.channels = channels
         if num_head_channels == -1:
             self.num_heads = num_heads

src/model/inception_resnet_v2/diffusion/diff_ae/diffusion_layer_swin_2d.py
@@ -866,7 +866,7 @@ class BasicDecodeLayer(nn.Module):
         self.emb_block_list = get_emb_block_list(act_layer, emb_dim_list, emb_type_list, dim)
         self.emb_type_list = emb_type_list
         # build blocks
-        self.mlp_after_skip = Mlp(in_features=dim + skip_dim, 
+        self.mlp_after_skip = Mlp(in_features=dim + skip_dim,
                                   out_features=dim,
                                   act_layer=act_layer, drop=drop)
         self.blocks_after_skip = nn.ModuleList([

src/model/inception_resnet_v2/diffusion/diff_ae/model_multi_task_swin.py
@@ -6,7 +6,7 @@ from torch import nn
 from einops.layers.torch import Rearrange
 from .nn import timestep_embedding
 from ...common_module.layers import get_act
-from .diffusion_layer import OutputND, ConvBlockND
+from .diffusion_layer import OutputND, ConvBlockND, AttentionBlock
 from src.model.swin_transformer.model_2d.swin_layers import Mlp
 from .diffusion_layer_swin_2d import AttentionPool1d, RMSNorm
 from .diffusion_layer_swin_2d import BasicLayerV1, BasicLayerV2
@@ -16,8 +16,8 @@ from .diffusion_layer_swin_2d import PatchEmbed, PatchMerging, PatchExpanding, B
 from einops import rearrange, repeat
 
 def get_time_emb_dim(emb_dim):
-    time_emb_dim_init = emb_dim // 2
-    time_emb_dim = emb_dim * 4
+    time_emb_dim = max(512, emb_dim * 8)
+    time_emb_dim_init = time_emb_dim // 4
     return time_emb_dim_init, time_emb_dim
 
 class SwinMultitask(nn.Module):
@@ -31,8 +31,8 @@ class SwinMultitask(nn.Module):
                 window_sizes=[8, 4, 4, 2], mlp_ratio=4., qkv_bias=True, ape=True, patch_norm=True,
                 drop_rate=0., attn_drop_rate=0., drop_path_rate=0.0,
                 use_checkpoint=False, pretrained_window_sizes=0,
-                get_seg=True, get_class=False, get_recon=False, get_validity=False,
-                get_diffusion=False, include_encoder=True, include_latent_net=True):
+                get_diffusion=False, get_seg=True, get_class=False, get_recon=False, get_validity=False,
+                include_encoder=False, include_latent_net=False):
         super().__init__()
         norm_layer_list = ["rms", "group"]
         assert norm_layer in norm_layer_list, f"you can choose norm layer: {norm_layer_list}"
@@ -65,32 +65,13 @@ class SwinMultitask(nn.Module):
         self.get_seg = get_seg
         self.get_recon = get_recon
         self.get_validity = get_validity
+        self.get_diffusion = get_diffusion
         ##################################
         self.num_layers = len(depths)
         self.ape = ape
         self.num_features = int(embed_dim * 2 ** (self.num_layers - 1))
         self.feature_dim = int(self.embed_dim * 2 ** self.num_layers)
 
-        if num_class_embeds is not None:
-            class_emb_dim = embed_dim * 4
-            self.class_emb_layer = nn.Embedding(num_class_embeds, class_emb_dim)
-            if self.cond_drop_prob > 0:
-                self.null_class_emb = nn.Parameter(torch.randn(class_emb_dim))
-            else:
-                self.null_class_emb = None
-            
-            self.class_mlp = nn.Sequential(
-                nn.Linear(class_emb_dim, class_emb_dim),
-                get_act(self.model_act_layer),
-                nn.Linear(class_emb_dim, class_emb_dim)
-            )
-            emb_dim_list = [class_emb_dim]
-        else:
-            self.class_emb_layer = None
-            self.null_class_emb = None
-            self.class_mlp = None
-            emb_dim_list = []
-        self.emb_dim_list = emb_dim_list
         # stochastic depth
         self.dpr = [x.item() for x in torch.linspace(0, drop_path_rate,
                                                 sum(depths))]  # stochastic depth decay rule
@@ -115,14 +96,45 @@ class SwinMultitask(nn.Module):
         self.encode_layers = self.get_encode_layers()
         self.mid_layer_1, self.mid_attn, self.mid_layer_2 = self.get_mid_layer()
         
+        emb_dim_list = []
+        emb_type_list = []
+        time_emb_dim_init, time_emb_dim = get_time_emb_dim(embed_dim)
         if get_diffusion:
-            time_emb_dim_init, time_emb_dim = get_time_emb_dim(embed_dim)
             self.time_emb_dim_init = time_emb_dim_init
             self.time_mlp = nn.Sequential(
                 nn.Linear(time_emb_dim_init, time_emb_dim),
                 get_act(act_layer),
                 nn.Linear(time_emb_dim, time_emb_dim)
             )
+            emb_dim_list.append(time_emb_dim)
+            emb_type_list.append("seq")
+
+            self.diff_decode_layers = self.get_decode_layers(is_diffusion=True)
+            self.diff_final_layer, self.diff_final_expanding, self.diff_out_conv = self.get_decode_final_layers(seg_out_chans, seg_out_act, is_diffusion=True)
+
+        if num_class_embeds is not None:
+            class_emb_dim = time_emb_dim
+            self.class_emb_layer = nn.Embedding(num_class_embeds, class_emb_dim)
+            if self.cond_drop_prob > 0:
+                self.null_class_emb = nn.Parameter(torch.randn(class_emb_dim))
+            else:
+                self.null_class_emb = None
+            
+            self.class_mlp = nn.Sequential(
+                nn.Linear(class_emb_dim, class_emb_dim),
+                get_act(self.model_act_layer),
+                nn.Linear(class_emb_dim, class_emb_dim)
+            )
+            emb_dim_list.append(class_emb_dim)
+            emb_type_list.append("seq")
+        else:
+            self.class_emb_layer = None
+            self.null_class_emb = None
+            self.class_mlp = None
+            emb_dim_list = []
+        self.emb_dim_list = emb_dim_list
+        self.emb_type_list = emb_type_list
+
         if get_seg:
             self.seg_decode_layers = self.get_decode_layers()
             self.seg_final_layer, self.seg_final_expanding, self.seg_out_conv = self.get_decode_final_layers(seg_out_chans, seg_out_act)
@@ -166,21 +178,31 @@ class SwinMultitask(nn.Module):
     def no_weight_decay_keywords(self):
         return {"cpb_mlp", "logit_scale", 'relative_position_bias_table'}
 
-    def forward(self, x, class_labels=None,
-                cond_drop_prob=None):
+    def forward(self, x, t, t_cond=None,
+                x_start=None, cond=None,
+                x_self_cond=None, class_labels=None, cond_drop_prob=None):
         output = []
+        emb_list = []
+        if self.get_diffusion:
+            time_emb = timestep_embedding(t, self.time_emb_dim_init)
+            time_emb = self.time_mlp(time_emb)
+
         if self.num_class_embeds is not None:
             class_emb = self.process_class_emb(x, class_labels, cond_drop_prob)
-            emb_list = [class_emb]
-        else:
-            emb_list = []
-
-        x, skip_connect_list = self.process_encode_layers(x, emb_list)
-        x = self.process_mid_layers(x, emb_list)
+            emb_list.append(class_emb)
+        if self.get_diffusion:
+            diff_x, diff_skip_connect_list = self.process_encode_layers(x, emb_list)
+            diff_x = self.process_mid_layers(diff_x, emb_list)
+            diff_output = self.process_decode_layers(diff_x, self.diff_decode_layers,
+                                                    self.diff_final_layer, self.diff_final_expanding, self.diff_out_conv,
+                                                    diff_skip_connect_list, emb_list)
+            output.append(diff_output)
+        x, skip_connect_list = self.process_encode_layers(x, emb_list[-1:])
+        x = self.process_mid_layers(x, emb_list[-1:])
         if self.get_seg:
             seg_output = self.process_decode_layers(x, self.seg_decode_layers,
                                                     self.seg_final_layer, self.seg_final_expanding, self.seg_out_conv,
-                                                    skip_connect_list, emb_list)
+                                                    skip_connect_list, emb_list[-1:])
             output.append(seg_output)
         if self.get_class:
             class_output = self.class_head(x)
@@ -188,7 +210,7 @@ class SwinMultitask(nn.Module):
         if self.get_recon:
             recon_output = self.process_decode_layers(x, self.recon_decode_layers,
                                                       self.recon_final_layer, self.recon_final_expanding, self.recon_out_conv,
-                                                      skip_connect_list, emb_list)
+                                                      skip_connect_list, emb_list[-1:])
             output.append(recon_output)
         if self.get_validity:
             validitiy_output = self.validity_head(x)
@@ -256,8 +278,8 @@ class SwinMultitask(nn.Module):
             emb_dim_list = self.emb_dim_list
             emb_type_list = self.emb_type_list
         else:
-            emb_dim_list = []
-            emb_type_list = []
+            emb_dim_list = self.emb_dim_list[-1:]
+            emb_type_list = self.emb_type_list[-1:]
 
         common_kwarg_dict = {"dim":dim,
                             "input_resolution":feature_resolution,
@@ -270,14 +292,14 @@ class SwinMultitask(nn.Module):
                             "norm_layer":self.norm_layer,
                             "act_layer":get_act(self.model_act_layer),
                             "pretrained_window_size":self.pretrained_window_sizes[i_layer],
-                            "use_checkpoint":self.use_checkpoint[i_layer]
+                            "use_checkpoint":self.use_checkpoint[i_layer],
                             "emb_dim_list": emb_dim_list,
                             "emb_type_list": emb_type_list 
                             }
         return common_kwarg_dict
     
     def get_init_layer(self):
-        common_kwarg_dict = self.get_layer_config_dict(self.embed_dim, self.patches_resolution, 0)
+        common_kwarg_dict = self.get_layer_config_dict(self.embed_dim, self.patches_resolution, 0, is_diffusion=True)
         init_layer = BasicLayerV2(**common_kwarg_dict)
         return init_layer
     
@@ -286,7 +308,7 @@ class SwinMultitask(nn.Module):
         for i_layer in range(self.num_layers):
             layer_dim = int(self.embed_dim * 2 ** i_layer)
             feature_resolution = np.array(self.patches_resolution) // (2 ** i_layer)
-            common_kwarg_dict = self.get_layer_config_dict(layer_dim, feature_resolution, i_layer)
+            common_kwarg_dict = self.get_layer_config_dict(layer_dim, feature_resolution, i_layer, is_diffusion=True)
             common_kwarg_dict["downsample"] = PatchMerging
             encode_layer = BasicLayerV2(**common_kwarg_dict)
             encode_layers.append(encode_layer)
@@ -295,12 +317,12 @@ class SwinMultitask(nn.Module):
     def get_mid_layer(self):
         i_layer = self.num_layers - 1
         feature_dim = self.feature_dim
-        common_kwarg_dict = self.get_layer_config_dict(feature_dim, self.feature_hw, i_layer)
+        common_kwarg_dict = self.get_layer_config_dict(feature_dim, self.feature_hw, i_layer, is_diffusion=True)
         mid_layer_1 = BasicLayerV2(**common_kwarg_dict)
         mid_attn_norm_layer = "instance" if self.norm_layer == "instance" else "rms"
         # TBD: self.attn_drop_rate 추가할지 고민
-        mid_attn = Attention(dim=feature_dim, num_heads=self.num_heads[i_layer],
-                            use_checkpoint=self.use_checkpoint[i_layer], norm_layer=mid_attn_norm_layer)
+        mid_attn = AttentionBlock(channels=feature_dim, num_heads=common_kwarg_dict["num_heads"],
+                                  use_checkpoint=common_kwarg_dict["use_checkpoint"], norm_layer=mid_attn_norm_layer)
         mid_layer_2 = BasicLayerV2(**common_kwarg_dict)
         return mid_layer_1, mid_attn, mid_layer_2
     

src/model/inception_resnet_v2/diffusion/diff_ae/renderer.py
@@ -2,10 +2,12 @@ import torch
 from torch.cuda import amp
 from .model import z_normalize
 import os
-def render_uncondition(model, x_T, sampler, latent_sampler,
+from .diffusion_sampler import GaussianSampler
+def render_uncondition(model, x_T, sampler: GaussianSampler, latent_sampler: GaussianSampler,
                        clip_latent_noise: bool = False,
                        latent_clip_sample=False,
-                       latent_znormalize=True,
+                       cond_mean=None,
+                       cond_std=None,
                        train_mode=None,
                        clip_denoised=True,
                        image_mask_cat_fn=None,
@@ -24,12 +26,11 @@ def render_uncondition(model, x_T, sampler, latent_sampler,
             noise=latent_noise,
             clip_denoised=latent_clip_sample,
         )
-        if latent_znormalize:
-            cond = z_normalize(cond)
-
+        if (cond_mean is not None) and (cond_std is not None):
+            cond = cond * cond_std + cond_mean 
         # the diffusion on the model
         return sampler.sample(model=model, image_mask_cat_fn=image_mask_cat_fn, image_mask_split_fn=image_mask_split_fn,
-                              noise=x_T, cond=cond, clip_denoised=clip_denoised)
+                              noise=x_T, model_kwargs={'cond': cond}, clip_denoised=clip_denoised)
     else:
         raise NotImplementedError()
 
@@ -77,7 +78,7 @@ def render_condition(
     image_mask_cat_fn=None,
     image_mask_split_fn=None
 ):
-    if train_mode in ["autoencoder", "autoencoder_latent_net"]:
+    if train_mode in ["autoencoder", "autoencoder_latent_net", "latent_net"]:
         if cond is None:
             cond = model.encode(x_start)
         return sampler.sample(model=model,