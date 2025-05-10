215a69b5bc56789c4299d5e387679e8a640c23b6
tobeor3009 <tobeor3009@gmail.com>
2025-05-10 21:41:47 +0900

    daily commit

--- src/model/inception_resnet_v2/diffusion/diff_ae/model_multi_task_swin_bak.py
+++ src/model/inception_resnet_v2/diffusion/diff_ae/model_multi_task_swin_bak.py
@@ -5,43 +5,15 @@ from functools import partial
 from torch import nn
 from einops.layers.torch import Rearrange
 from .nn import timestep_embedding
-from .diffusion_layer import OutputND, ConvBlockND, Return, GroupNorm32, MultiDecoderND_V2, ResNetBlockND
+from .diffusion_layer import OutputND, ConvBlockND, Return, GroupNorm32
 from src.model.swin_transformer.model_2d.swin_layers import Mlp
-from .diffusion_layer_swin_2d import AttentionPool1d, AttentionBlock
+from .diffusion_layer_swin_2d import AttentionPool1d, RMSNorm, AttentionBlock
 from .diffusion_layer_swin_2d import BasicLayerV1, BasicLayerV2, MeanBN2BC
-from .diffusion_layer_swin_2d import default, prob_mask_like, get_act
+from .diffusion_layer_swin_2d import default, prob_mask_like, get_act, get_norm
 from .diffusion_layer_swin_2d import PatchEmbed, PatchMerging, PatchExpanding
 from .diffusion_layer_swin_2d import SkipEncodeLayer, BasicDecodeLayer
 from .model import MLPSkipNet
 from einops import rearrange, repeat
-from torch.nn import functional as F
-from einops.layers.torch import Rearrange
-
-class RMSNorm(nn.Module):
-    def __init__(self, dim, img_dim=0):
-        super().__init__()
-        # img_dim = 0: sequence [B, N, D], img_dim = 1: 1d, img_dim = 2: 2d, img_dim = 3: 3d
-        if img_dim == 0:
-            param_shape = (1, 1, dim)
-            self.normalize_dim = 2
-        elif img_dim == 1:
-            param_shape = (1, dim, 1)
-            self.normalize_dim = 1
-        elif img_dim == 2:
-            param_shape = (1, dim, 1, 1)
-            self.normalize_dim = 1
-        elif img_dim == 3:
-            param_shape = (1, dim, 1, 1, 1)
-            self.normalize_dim = 1
-        self.weight = nn.Parameter(torch.ones(*param_shape))
-        self.scale = dim ** 0.5
-    def forward(self, x):
-        return F.normalize(x, dim=self.normalize_dim) * self.weight * self.scale
-
-def get_rms_norm_nd(img_dim):
-    norm_layer = partial(RMSNorm, img_dim=img_dim)
-    return norm_layer
-
 
 def get_time_emb_dim(emb_dim):
     # time_emb_dim = max(512, emb_dim * 8)
@@ -53,7 +25,7 @@ def get_time_emb_dim(emb_dim):
 class SwinMultitask(nn.Module):
     def __init__(self, img_size=512, patch_size=4,
                  in_channel=3, cond_channel=3, self_condition=False,
-                 act_layer="silu", emb_channel=1024,
+                 norm_layer="rms", act_layer="silu", emb_channel=1024,
                  diffusion_out_channel=1, diffusion_out_act=None, diffusion_decode_fn_str="pixel_shuffle",
                  seg_out_channel=2, seg_out_act="softmax", seg_decode_fn_str="pixel_shuffle",
                  num_classes=1000, class_act="softmax", recon_act="sigmoid", recon_decode_fn_str="pixel_shuffle",
@@ -67,12 +39,12 @@ class SwinMultitask(nn.Module):
                 include_encoder=False, include_latent_net=False,
                 encoder_unet=False, encoder_unet_out_channel=3, encoder_unet_out_act="softmax"):
         super().__init__()
-        self.img_dim = 2
-        self.seq_norm_layer = get_rms_norm_nd(img_dim=0)
-        self.nd_norm_layer = get_rms_norm_nd(img_dim=self.img_dim)
+        norm_layer_list = ["rms", "group"]
+        assert norm_layer in norm_layer_list, f"you can choose norm layer: {norm_layer_list}"
+        self.norm_layer = norm_layer
         self.model_act_layer = act_layer
         patch_size = int(patch_size)
-        self.patch_expand_conv_dim = int(np.log2(patch_size))
+        self.img_dim = 2
         # for compability with diffusion_sample
         self.img_size = img_size
         self.cond_channel = cond_channel
@@ -127,7 +99,7 @@ class SwinMultitask(nn.Module):
         # split image into non-overlapping patches
         self.patch_embed = PatchEmbed(img_size=img_size, patch_size=patch_size,
                                     in_chans=in_channel, embed_dim=embed_dim,
-                                    norm_layer=self.seq_norm_layer if self.patch_norm else None)
+                                    norm_layer=self.norm_layer if self.patch_norm else None)
         
         num_patches = self.patch_embed.num_patches
         patches_resolution = self.patch_embed.patches_resolution
@@ -161,7 +133,7 @@ class SwinMultitask(nn.Module):
                 else:
                     self.encoder = SwinEncoder(img_size=img_size, patch_size=patch_size,
                                                 in_channel=cond_channel, emb_channel=emb_channel, self_condition=False,
-                                                act_layer=act_layer,
+                                                norm_layer=norm_layer, act_layer=act_layer,
                                                 embed_dim=embed_dim, depths=depths, num_heads=num_heads,
                                                 window_sizes=window_sizes, mlp_ratio=mlp_ratio,
                                                 qkv_bias=qkv_bias, ape=ape, patch_norm=patch_norm,
@@ -444,6 +416,7 @@ class SwinMultitask(nn.Module):
         for decode_idx, decode_layer in enumerate(decode_layers, start=0):
             skip_x = skip_connect_list[decode_idx]
             x = decode_layer(x, skip_x, *emb_list)
+
         x = decode_final_layer_1(x, skip_connect_list[-1], *emb_list)
         x = decode_final_layer_2(x, *emb_list)
         x = decode_final_expanding(x)
@@ -472,7 +445,7 @@ class SwinMultitask(nn.Module):
                             "mlp_ratio":self.mlp_ratio, "qkv_bias":self.qkv_bias, 
                             "drop":self.drop_rate, "attn_drop":self.attn_drop_rate,
                             "drop_path":self.dpr[sum(depths[:i_layer]):sum(depths[:i_layer + 1])],
-                            "norm_layer":self.seq_norm_layer,
+                            "norm_layer":self.norm_layer,
                             "act_layer":get_act(self.model_act_layer),
                             "pretrained_window_size":self.pretrained_window_sizes[i_layer],
                             "use_checkpoint":self.use_checkpoint[i_layer],
@@ -508,7 +481,7 @@ class SwinMultitask(nn.Module):
         feature_dim = self.feature_dim
         common_kwarg_dict = self.get_layer_config_dict(feature_dim, self.feature_hw, i_layer, is_diffusion=True)
         mid_layer_1 = BasicLayerV1(**common_kwarg_dict)
-        mid_attn_norm_layer = self.seq_norm_layer
+        mid_attn_norm_layer = "group" if self.norm_layer == "group" else "rms"
         # TBD: self.attn_drop_rate 추가할지 고민
         mid_attn = AttentionBlock(channels=feature_dim, num_heads=common_kwarg_dict["num_heads"],
                                   use_checkpoint=common_kwarg_dict["use_checkpoint"], norm_layer=mid_attn_norm_layer)
@@ -535,25 +508,22 @@ class SwinMultitask(nn.Module):
         common_kwarg_dict = self.get_layer_config_dict(self.embed_dim, self.patches_resolution, i_layer, is_diffusion)
         decode_final_layer_1 = BasicDecodeLayer(skip_dim=self.embed_dim, upsample=None, **common_kwarg_dict)
         decode_final_layer_2 = BasicLayerV1(**common_kwarg_dict)
+        decode_final_expanding = PatchExpanding(input_resolution=self.patches_resolution,
+                                                    dim=self.embed_dim,
+                                                    return_vector=False,
+                                                    dim_scale=self.patch_size,
+                                                    norm_layer=self.norm_layer,
+                                                    decode_fn_str="pixel_shuffle",
+                                                    img_dim=self.img_dim
+                                                    )
         
-        view_layer = Rearrange(' b (h w) c -> b c h w', h=self.patches_resolution[0], w=self.patches_resolution[1])
-        decode_final_layer_list = [view_layer]
-        for _ in range(self.patch_expand_conv_dim):
-            decode_final_layer = nn.Sequential(
-                ResNetBlockND(self.embed_dim, self.embed_dim, kernel_size=3, padding=1, stride=1, norm=self.nd_norm_layer, act="silu"),
-                MultiDecoderND_V2(self.embed_dim, self.embed_dim, norm=self.nd_norm_layer, act="silu", kernel_size=2, dropout_proba=0.0,
-                                  decode_fn_str_list=["conv_transpose", "pixel_shuffle"], img_dim=self.img_dim, use_residual_conv=True)
-            )
-            decode_final_layer_list.append(decode_final_layer)
-
-        decode_final_expanding = nn.Sequential(*decode_final_layer_list)
-        decode_out_conv_1 = ConvBlockND(self.embed_dim, self.embed_dim,
+        decode_out_conv_1 = ConvBlockND(self.embed_dim // 2, self.embed_dim // 2,
                                         kernel_size=3, padding=1, stride=1,
-                                      norm=self.nd_norm_layer, act="silu", img_dim=self.img_dim)
-        decode_out_conv_2 = ConvBlockND(self.embed_dim, self.embed_dim,
+                                      norm=GroupNorm32, act="silu", img_dim=self.img_dim)
+        decode_out_conv_2 = ConvBlockND(self.embed_dim // 2, self.embed_dim // 2,
                                         kernel_size=3, padding=1, stride=1,
-                                      norm=self.nd_norm_layer, act="silu", img_dim=self.img_dim)
-        decode_out_conv_3 = OutputND(self.embed_dim, decode_out_channel, act=decode_out_act, img_dim=self.img_dim)
+                                      norm=GroupNorm32, act="silu", img_dim=self.img_dim)
+        decode_out_conv_3 = OutputND(self.embed_dim // 2, decode_out_channel, act=decode_out_act, img_dim=self.img_dim)
         decode_out_conv = nn.Sequential(decode_out_conv_1, decode_out_conv_2, decode_out_conv_3)
         
         return decode_final_layer_1, decode_final_layer_2, decode_final_expanding, decode_out_conv
@@ -563,7 +533,7 @@ class SwinMultitask(nn.Module):
         class_head = nn.Sequential(
                 get_act(self.model_act_layer),
                 AttentionPool1d(sequence_length=np.prod(self.feature_hw), embed_dim=feature_dim,
-                                num_heads=self.num_heads[-1], output_dim=feature_dim * 2, channel_first=False),
+                                num_heads=8, output_dim=feature_dim * 2, channel_first=False),
                 nn.Linear(feature_dim * 2, feature_dim),
                 nn.Dropout(p=0.1),
                 get_act(self.model_act_layer),
@@ -606,18 +576,17 @@ class SwinMultitask(nn.Module):
 class SwinEncoder(SwinMultitask):
     def __init__(self, img_size=512, patch_size=4,
                 in_channel=3, emb_channel=1024, self_condition=False,
-                act_layer="silu",
+                norm_layer="rms", act_layer="silu",
                 embed_dim=96, depths=[2, 2, 2, 2], num_heads=[3, 6, 12, 24],
                 window_sizes=[8, 4, 4, 2], mlp_ratio=4., qkv_bias=True, ape=True, patch_norm=True,
                 drop_rate=0., attn_drop_rate=0., drop_path_rate=0.0,
                 use_decoder=False, decode_out_channel=3,
                 decoder_out_act="softmax", use_checkpoint=False, pretrained_window_sizes=0):
         super(SwinMultitask, self).__init__()
-        self.img_dim = 2
-        self.seq_norm_layer = get_rms_norm_nd(img_dim=0)
-        self.nd_norm_layer = get_rms_norm_nd(img_dim=self.img_dim)
+        self.norm_layer = norm_layer
         self.model_act_layer = act_layer
         patch_size = int(patch_size)
+        self.img_dim = 2
         # for compability with diffusion_sample
         self.img_size = img_size
         self.self_condition = self_condition
@@ -656,7 +625,7 @@ class SwinEncoder(SwinMultitask):
         # split image into non-overlapping patches
         self.patch_embed = PatchEmbed(img_size=img_size, patch_size=patch_size,
                                     in_chans=in_channel, embed_dim=embed_dim,
-                                    norm_layer=self.seq_norm_layer if self.patch_norm else None)
+                                    norm_layer=self.norm_layer if self.patch_norm else None)
 
         num_patches = self.patch_embed.num_patches
         patches_resolution = self.patch_embed.patches_resolution
@@ -672,7 +641,7 @@ class SwinEncoder(SwinMultitask):
         self.encode_layers = self.get_encode_layers()
         self.mid_layer_1, self.mid_attn, self.mid_layer_2 = self.get_mid_layer()
         self.pool_layer = nn.Sequential(
-            self.seq_norm_layer,
+            get_norm(norm_layer, self.feature_dim),
             get_act(act_layer),
             MeanBN2BC(target_dim=1),
             nn.Flatten(start_dim=1),
